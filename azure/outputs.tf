7aebe04337b91cd372337b18bfcefc0596c2dd5e
credibleforce <jamiefrasermcmurray@gmail.com>
2022-02-15 11:24:07 -0800

    Update base

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -1,10 +1,10 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.default.name
-}
+# output "resource_group_name" {
+#   value = azurerm_resource_group.default.name
+# }
 
-output "kubernetes_cluster_name" {
-  value = azurerm_kubernetes_cluster.default.name
-}
+# output "kubernetes_cluster_name" {
+#   value = azurerm_kubernetes_cluster.default.name
+# }
 
 # output "host" {
 #   value = azurerm_kubernetes_cluster.default.kube_config.0.host
