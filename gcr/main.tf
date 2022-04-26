a8f7797a4e9ed45369c3479ba5973d340a6102ab
credibleforce <jamiefrasermcmurray@gmail.com>
2022-04-26 08:53:03 -0700

    update gcr example

---
+++ gcr/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    lacework = {
+      source = "lacework/lacework"
+    }
+  }
+}
+
+data "google_project" "project" {}
+
+data "google_projects" "projects" {
+  filter = "parent.id:${data.google_project.project.org_id} lifecycleState:ACTIVE"
+}
+
+provider "lacework" {
+  profile="proservlab"
+}
+
+provider "google" {}
+
+module "lacework_gcr" {
+  source  = "lacework/gcr/gcp"
+  version = "~> 1.0"
+  for_each   = { 
+    for index, project in data.google_projects.projects.projects: project.project_id => project
+  }
+  project_id = each.value.project_id
+  
+  lacework_integration_name = each.value.project_id
+
+  non_os_package_support="true"
+  limit_num_imgs="15"
+}
