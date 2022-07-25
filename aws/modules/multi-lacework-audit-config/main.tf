9aaaa315f737715f3dd70718594973b0b5ceafd7
credibleforce <jamiefrasermcmurray@gmail.com>
2022-07-24 23:11:05 -0700

    update providers

--- aws/modules/multi-lacework-audit-config/main.tf
+++ aws/modules/multi-lacework-audit-config/main.tf
@@ -25,7 +25,7 @@ module "aws_cloudtrail" {
   version = "~> 2.1.1"
 
   use_existing_cloudtrail   = false     
-  use_existing_iam_role     = false
+  use_existing_iam_role     = true
   #bucket_arn                = "arn:aws:s3:::aws-controltower-logs-538045531862-us-east-1/o-y3gcbsjf7l/AWSLogs/997124715511/"
   #bucket_name               = "bucket-name"
   #sns_topic_name             = "cloudtrail-multi-region-sns-topic"
--- aws/modules/multi-lacework-audit/main.tf
+++ aws/modules/multi-lacework-audit/main.tf
@@ -24,7 +24,4 @@ module "aws_cloudtrail" {
   #bucket_arn                = "arn:aws:s3:::aws-controltower-logs-538045531862-us-east-1/o-y3gcbsjf7l/AWSLogs/997124715511/"
   #bucket_name               = "bucket-name"
   #sns_topic_name             = "cloudtrail-multi-region-sns-topic"
-  iam_role_name             = module.aws_config.iam_role_name
-  iam_role_arn              = module.aws_config.iam_role_arn
-  iam_role_external_id      = module.aws_config.external_id
 }
