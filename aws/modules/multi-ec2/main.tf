666cf9734d9f7e9cccdaba7477b3107a7c983e56
credibleforce <jamiefrasermcmurray@gmail.com>
2022-07-25 23:26:47 -0700

    update main

--- aws/modules/multi-ec2/main.tf
+++ aws/modules/multi-ec2/main.tf
@@ -101,7 +101,7 @@ resource "aws_route_table" "main" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.main.id
+    nat_gateway_id = aws_nat_gateway.main.id
   }
 
   tags = {
