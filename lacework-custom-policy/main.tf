8328da9fd1109f3975bd51e4ccd50b71b1ae8983
credibleforce <jamiefrasermcmurray@gmail.com>
2022-08-18 21:39:50 -0700

    update examples

--- lacework-custom-policy/main.tf
+++ lacework-custom-policy/main.tf
@@ -1,7 +1,7 @@
 resource "lacework_query" "query1" {
   query_id = "TF_AWS_CTA_Example"
   query    = <<EOT
-  {
+  TF_AWS_CTA_Example {
     source {
         CloudTrailRawEvents
     }
@@ -38,3 +38,53 @@ resource "lacework_policy" "example1" {
     profile = "LW_CloudTrail_Alerts.CloudTrailDefaultAlert_AwsResource"
   }
 }
+
+resource "lacework_query" "query2" {
+  query_id = "AWS_Config_EC2InstanceNotTagged"
+  query    = <<EOT
+  AWS_Config_EC2InstanceNotTagged {
+      SOURCE {
+          LW_CFG_AWS
+      }
+      FILTER {
+          RESOURCE_TYPE = 'ec2:instance'
+          and API_KEY = 'describe-instances'
+          and RESOURCE_CONFIG:State.Name <> 'terminated'
+          and (not value_exists(RESOURCE_TAGS:environment)
+          or RESOURCE_TAGS = '{}')
+      }
+      RETURN DISTINCT {
+          ACCOUNT_ALIAS,
+          ACCOUNT_ID,
+          RESOURCE_CONFIG,
+          RESOURCE_ID,
+          RESOURCE_REGION,
+          RESOURCE_TYPE,
+          SERVICE,
+          'Ec2InstanceWithoutTags' as REASON
+      }
+  }
+EOT
+}
+
+resource "lacework_policy" "example2" {
+  title       = "Untagged EC2 Resources"
+  description = "Flags EC2 resources that do not have any tags on them"
+  remediation = "Use IaC provisioning for all cloud assets"
+  query_id    = lacework_query.query2.id
+  severity    = "High"
+  type        = "Violation"
+  evaluation  = "Hourly"
+  tags        = [
+    "subdomain:Configuration",
+    "domain:AWS"
+  ]
+  enabled     = true
+
+  alerting {
+    enabled = true
+    profile = "LW_CFG_AWS_DEFAULT_PROFILE.CFG_AWS_PolicyChanged"
+  }
+}
+
+
