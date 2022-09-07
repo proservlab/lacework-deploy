cbe7664141b8bbcb3f910067f9ce6a6d72c6b091
credibleforce <jamiefrasermcmurray@gmail.com>
2022-09-07 15:14:47 -0700

    update stitch

--- lacework-stitch/main.tf
+++ lacework-stitch/main.tf
@@ -3,34 +3,54 @@ provider "lacework" {
 }
 
 provider "aws" {
-  profile="proservlab"
+  profile = "proservlab"
   region = "us-east-1"
 }
 
-provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
-}
+# provider "aws" {
+#   profile = "proservlab"
+#   alias  = "usw2"
+#   region = "us-west-2"
+# }
 
-module "lacework_agentless_global" {
+module "lacework_aws_agentless_scanning_global" {
   source  = "lacework/agentless-scanning/aws"
-  version = "~> 0.1"
+  version = ">= 0.3.2"
 
   global                    = true
   lacework_integration_name = "sidekick_from_terraform"
 }
 
-module "lacework_aws_agentless_scanning_regional" {
+// Create regional resources in our first region
+module "lacework_aws_agentless_scanning_region" {
   source  = "lacework/agentless-scanning/aws"
-  version = "~> 0.1"
+  version = ">= 0.3.2"
 
-  providers = {
-    aws = aws.us-east-1
-  }
-
-  agentless_scan_ecs_task_role_arn      = module.lacework_agentless_global.agentless_scan_ecs_task_role_arn
-  agentless_scan_ecs_execution_role_arn = module.lacework_agentless_global.agentless_scan_ecs_execution_role_arn
-  agentless_scan_ecs_event_role_arn     = module.lacework_agentless_global.agentless_scan_ecs_event_role_arn
-  agentless_scan_secret_arn             = module.lacework_agentless_global.agentless_scan_secret_arn
-  lacework_account                      = module.lacework_agentless_global.lacework_account
+  regional                              = true
+  agentless_scan_ecs_task_role_arn      = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_task_role_arn
+  agentless_scan_ecs_execution_role_arn = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_execution_role_arn
+  agentless_scan_ecs_event_role_arn     = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_event_role_arn
+  agentless_scan_secret_arn             = module.lacework_aws_agentless_scanning_global.agentless_scan_secret_arn
+  lacework_account                      = module.lacework_aws_agentless_scanning_global.lacework_account
+  prefix                                = module.lacework_aws_agentless_scanning_global.prefix
+  suffix                                = module.lacework_aws_agentless_scanning_global.suffix
 }
+
+// Create regional resources in our second region
+# module "lacework_aws_agentless_scanning_region_usw2" {
+#   source  = "lacework/agentless-scanning/aws"
+#   version = ">= 0.3.2"
+
+#   providers = {
+#     aws = aws.usw2
+#   }
+
+#   regional                              = true
+#   agentless_scan_ecs_task_role_arn      = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_task_role_arn
+#   agentless_scan_ecs_execution_role_arn = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_execution_role_arn
+#   agentless_scan_ecs_event_role_arn     = module.lacework_aws_agentless_scanning_global.agentless_scan_ecs_event_role_arn
+#   agentless_scan_secret_arn             = module.lacework_aws_agentless_scanning_global.agentless_scan_secret_arn
+#   lacework_account                      = module.lacework_aws_agentless_scanning_global.lacework_account
+#   prefix                                = module.lacework_aws_agentless_scanning_global.prefix
+#   suffix                                = module.lacework_aws_agentless_scanning_global.suffix
+# }
