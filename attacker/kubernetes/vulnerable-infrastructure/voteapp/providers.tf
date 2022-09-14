1db903446ca9cffa96a514225e51f69bb7d963f5
credibleforce <jamiefrasermcmurray@gmail.com>
2022-09-14 06:24:24 -0700

    update attacker

---
+++ attacker/kubernetes/vulnerable-infrastructure/voteapp/providers.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.12.1"
+    }
+    lacework = {
+      source  = "lacework/lacework"
+      version = "~> 0.22.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.19.0"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+}
+
+provider "aws" {
+  profile = "${var.environment}"
+  region  = "${var.region}"
+
+  # Make it faster by skipping something
+  skip_get_ec2_platforms      = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
+
+data "aws_caller_identity" "current" {}
