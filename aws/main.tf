297fcf6b64f03682298e57bea483ef646d283a3c
credibleforce <jamiefrasermcmurray@gmail.com>
2022-10-04 19:33:59 -0700

    update main

--- aws/main.tf
+++ aws/main.tf
@@ -4,11 +4,11 @@ module "environment-proservlab" {
   region      = var.region
 
   # slack
-  enable_slack_alerts = true
+  enable_slack_alerts = false
   slack_token         = var.slack_token
 
   # jira
-  enable_jira_cloud_alerts = true
+  enable_jira_cloud_alerts = false
   jira_cloud_url           = var.jira_cloud_url
   jira_cloud_project_key   = var.jira_cloud_project_key
   jira_cloud_issue_type    = var.jira_cloud_issue_type
@@ -19,10 +19,10 @@ module "environment-proservlab" {
   cluster_name = var.cluster_name
 
   # aws core environment
-  enable_ec2     = true
-  enable_eks     = true
-  enable_eks_app = true
-  enable_eks_psp = true
+  enable_ec2     = false
+  enable_eks     = false
+  enable_eks_app = false
+  enable_eks_psp = false
 
   # kubernetes admission controller
   proxy_token = var.proxy_token
@@ -31,17 +31,17 @@ module "environment-proservlab" {
   lacework_agent_access_token           = var.lacework_agent_access_token
   lacework_server_url                   = var.lacework_server_url
   lacework_account_name                 = var.lacework_account_name
-  enable_lacework_alerts                = true
-  enable_lacework_audit_config          = true
-  enable_lacework_custom_policy         = true
-  enable_lacework_daemonset             = true
-  enable_lacework_daemonset_compliance  = true
-  enable_lacework_agentless             = true
-  enable_lacework_ssm_deployment        = true
-  enable_lacework_admissions_controller = true
+  enable_lacework_alerts                = false
+  enable_lacework_audit_config          = false
+  enable_lacework_custom_policy         = false
+  enable_lacework_daemonset             = false
+  enable_lacework_daemonset_compliance  = false
+  enable_lacework_agentless             = false
+  enable_lacework_ssm_deployment        = false
+  enable_lacework_admissions_controller = false
 
   # attack
-  enable_attack_kubernetes_voteapp = true
+  enable_attack_kubernetes_voteapp = false
 
   providers = {
     aws        = aws.main
