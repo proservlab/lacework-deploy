6ce2d9ee54093f047f736be066481f3907ca0d21
credibleforce <jamiefrasermcmurray@gmail.com>
2022-10-25 11:58:45 -0700

    fix inputs

--- aws/modules/attacksurface-agentless-secrets/inputs.tf
+++ aws/modules/attacksurface-agentless-secrets/inputs.tf
@@ -2,7 +2,7 @@ variable "environment" {
   type    = string
 }
 
-variable "resource_query_deploy_malware_eicar" {
+variable "resource_query_deploy_secret_ssh_private" {
     type    = object({
       ResourceTypeFilters = list(string)
       TagFilters  = list(object({
@@ -18,7 +18,32 @@ variable "resource_query_deploy_malware_eicar" {
 
                 TagFilters = [
                     {
-                        Key = "ssm_deploy_malware_eicar"
+                        Key = "ssm_deploy_secret_ssh_private"
+                        Values = [
+                            "true"
+                        ]
+                    }
+                ]
+              }
+}
+
+variable "resource_query_deploy_secret_ssh_public" {
+    type    = object({
+      ResourceTypeFilters = list(string)
+      TagFilters  = list(object({
+        Key = string
+        Values = list(string)
+      }))
+    })
+    description = "JSON query to idenfity resources which will have lacework deployed"
+    default = {
+                ResourceTypeFilters = [
+                    "AWS::EC2::Instance"
+                ]
+
+                TagFilters = [
+                    {
+                        Key = "ssm_deploy_secret_ssh_public"
                         Values = [
                             "true"
                         ]
