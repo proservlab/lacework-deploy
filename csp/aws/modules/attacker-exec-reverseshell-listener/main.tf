c73a9bbdc13cd11b1bd33aafa587a6ccb586f693
credibleforce <jamiefrasermcmurray@gmail.com>
2022-11-22 10:05:34 -0800

    update inspector

---
+++ csp/aws/modules/attacker-exec-reverseshell-listener/main.tf
@@ -0,0 +1,70 @@
+locals {
+    port = 4444
+    pid_path = "/var/run/nc_listener"
+}
+
+resource "aws_ssm_document" "exec_reverse_shell" {
+  name          = "exec_reverse_shell"
+  document_type = "Command"
+
+  content = jsonencode(
+    {
+        "schemaVersion": "2.2",
+        "description": "exec reverse shell",
+        "mainSteps": [
+            {
+                "action": "aws:runShellScript",
+                "name": "exec_reverse_shell",
+                "precondition": {
+                    "StringEquals": [
+                        "platformType",
+                        "Linux"
+                    ]
+                },
+                "inputs": {
+                    "timeoutSeconds": "60",
+                    "runCommand": [
+                        "kill -9 $(cat ${local.pid_path}) 2>&1 > /dev/null",
+                        "/usr/bin/nc -l ${local.port} &",
+                        "echo -n $! > ${local.pid_path}",
+                        "touch /tmp/attacker_exec_reverseshell_listener",
+                    ]
+                }
+            }
+        ]
+    })
+}
+
+resource "aws_resourcegroups_group" "exec_reverse_shell" {
+    name = "exec_reverse_shell"
+
+    resource_query {
+        query = jsonencode(var.resource_query_exec_reverse_shell)
+    }
+
+    tags = {
+        billing = var.environment
+        owner   = "lacework"
+    }
+}
+
+resource "aws_ssm_association" "exec_reverse_shell" {
+    association_name = "exec_reverse_shell"
+
+    name = aws_ssm_document.exec_reverse_shell.name
+
+    targets {
+        key = "resource-groups:Name"
+        values = [
+            aws_resourcegroups_group.exec_reverse_shell.name,
+        ]
+    }
+
+    compliance_severity = "HIGH"
+
+    # every 30 minutes
+    schedule_expression = "cron(0/30 * * * ? *)"
+    
+    # will apply when updated and interval when false
+    apply_only_at_cron_interval = false
+}
--- csp/aws/modules/inspector/inputs.tf
+++ csp/aws/modules/inspector/inputs.tf
@@ -1,3 +1,12 @@
 variable "environment" {
   type    = string
 }
+
+variable "enabled_rules" {
+  type        = list(string)
+  default     = ["cve"]
+  description = <<-DOC
+    A list of AWS Inspector rules that should run on a periodic basis.
+    Valid values are `cve`, `cis`, `nr`, `sbp` which map to the appropriate [Inspector rule arns by region](https://docs.aws.amazon.com/inspector/latest/userguide/inspector_rules-arns.html).
+  DOC
+}
