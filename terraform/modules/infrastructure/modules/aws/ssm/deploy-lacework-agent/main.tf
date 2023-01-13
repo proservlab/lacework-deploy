30838f99a59d642e2aa4c54814e2af5bf9239ed8
credibleforce <jamiefrasermcmurray@gmail.com>
2023-01-13 00:23:18 -0800

    update ssm with unique resource names for lacework

--- terraform/modules/infrastructure/modules/aws/ssm/deploy-lacework-agent/main.tf
+++ terraform/modules/infrastructure/modules/aws/ssm/deploy-lacework-agent/main.tf
@@ -8,7 +8,9 @@ resource "lacework_agent_access_token" "agent" {
 
 module "lacework_aws_ssm_agents_install" {
     source  = "lacework/ssm-agent/aws"
-    version = "~> 0.7"
+    version = "~> 0.8"
+
+    aws_resources_prefix = "${var.environment}-${var.deployment}"
 
     # tags to add to the lacework data collector
     lacework_agent_tags = {
