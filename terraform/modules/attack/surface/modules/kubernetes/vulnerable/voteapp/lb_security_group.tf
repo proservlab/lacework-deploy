05d63562639fa677529a8a85608d370a2ba4a66f
credibleforce <jamiefrasermcmurray@gmail.com>
2023-01-14 19:38:46 -0800

    update lb allow inbound to vote ports

--- terraform/modules/attack/surface/modules/kubernetes/vulnerable/voteapp/lb_security_group.tf
+++ terraform/modules/attack/surface/modules/kubernetes/vulnerable/voteapp/lb_security_group.tf
@@ -28,3 +28,27 @@ resource "aws_security_group" "app_lb" {
     Name = "${var.app}-allow-lb-inbound"
   }
 }
+
+resource "aws_security_group_rule" "target_ingress_service_port_result" {
+  count = length(var.trusted_target_source)
+
+  type              = "ingress"
+  from_port         = 8002
+  to_port           = 8002
+  protocol          = "tcp"
+  cidr_blocks       = "0.0.0.0/0"
+  description       = "Allow all tcp inbound from to service port"
+  security_group_id = aws_security_group.app_lb.id
+}
+
+resource "aws_security_group_rule" "target_ingress_service_port_vote" {
+  count = length(var.trusted_target_source)
+
+  type              = "ingress"
+  from_port         = 8001
+  to_port           = 8001
+  protocol          = "tcp"
+  cidr_blocks       = "0.0.0.0/0"
+  description       = "Allow all tcp inbound from to service port"
+  security_group_id = aws_security_group.app_lb.id
+}
