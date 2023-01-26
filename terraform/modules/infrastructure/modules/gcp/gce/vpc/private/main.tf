6f47cf1e6b3dcdd4727e116669aec375036d0b98
credibleforce <jamiefrasermcmurray@gmail.com>
2023-01-25 20:35:35 -0800

    gcp update

--- terraform/modules/infrastructure/modules/gcp/gce/vpc/private/main.tf
+++ terraform/modules/infrastructure/modules/gcp/gce/vpc/private/main.tf
@@ -8,6 +8,7 @@ resource "google_compute_router" "router" {
 resource "google_compute_router_nat" "nat" {
     name                                = "main-${var.environment}-${var.deployment}-private-router-nat"
     router                              = google_compute_router.router.name
+    project                             = var.gcp_project_id
     region                              = google_compute_router.router.region
     nat_ip_allocate_option              = "AUTO_ONLY"
     source_subnetwork_ip_ranges_to_nat  = "ALL_SUBNETWORKS_ALL_IP_RANGES"
