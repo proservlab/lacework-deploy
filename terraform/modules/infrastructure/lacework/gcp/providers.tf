20750ea3b038ba50eeb334dd76b4cdec1c494928
credibleforce <jamiefrasermcmurray@gmail.com>
2023-02-06 14:59:20 -0800

    update provider place holder

--- terraform/modules/infrastructure/lacework/gcp/providers.tf
+++ terraform/modules/infrastructure/lacework/gcp/providers.tf
@@ -33,4 +33,7 @@ provider "helm" {
 
 provider "lacework" {
   profile = var.config.context.lacework.profile_name
+  account    = can(length(var.config.context.lacework.profile_name)) ? null : "my-account"
+  api_key    = can(length(var.config.context.lacework.profile_name)) ? null : "my-api-key"
+  api_secret = can(length(var.config.context.lacework.profile_name)) ? null : "my-api-secret"
 }
