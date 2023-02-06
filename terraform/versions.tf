10d11eafea2199325d564d55586ce5c1f1c2633c
credibleforce <jamiefrasermcmurray@gmail.com>
2023-02-06 15:07:02 -0800

    remove template provider requirement

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 0.15.0"
   required_providers {
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
-    }
     utils = {
       source  = "cloudposse/utils"
       version = "1.6.0"
