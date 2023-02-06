809b17ee121eea2a779720dc3d9233134371824d
credibleforce <jamiefrasermcmurray@gmail.com>
2023-02-06 09:06:46 -0800

    update inputs

--- terraform/modules/infrastructure/dynu/inputs.tf
+++ terraform/modules/infrastructure/dynu/inputs.tf
@@ -6,3 +6,11 @@ variable "config" {
   type = any
   description = "Schema defined in modules/context/infrastructure"
 }
+
+variable "dynu_api_token" {
+    type = string
+}
+
+variable "dynu_dns_domain" {
+    type = string
+}
