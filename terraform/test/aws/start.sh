eadbfb28fec535e2c27a46c0003d240cb2aced8b
credibleforce <jamiefrasermcmurray@gmail.com>
2023-02-08 16:01:35 -0800

    update test and add gke

--- terraform/test/aws/start.sh
+++ terraform/test/aws/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run --name=deploy-aws -w /workspace/terraform -it -v scenarios:/workspace/terraform/scenarios -v $HOME/.aws:/root/.aws -v $HOME/.lacework.toml:/root/.lacework.toml deploy-aws:latest
+docker stop deploy-aws
+docker rm deploy-aws
+
+docker run -d --name=deploy-aws -w /workspace/terraform -it -v "${PWD}/scenarios":/workspace/terraform/scenarios -v $HOME/.aws:/root/.aws -v $HOME/.lacework.toml:/root/.lacework.toml deploy-aws:latest \
+&& docker exec -it deploy-aws /bin/bash
--- terraform/test/gcp/start.sh
+++ terraform/test/gcp/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run --name=deploy-gcp -w /workspace/terraform -v scenarios:/workspace/terraform/scenarios -it -v $HOME/.lacework.toml:/root/.lacework.toml deploy-gcp:latest
+docker stop deploy-gcp
+docker rm deploy-gcp
+
+docker run -d --name=deploy-gcp -w /workspace/terraform -v "${PWD}/scenarios":/workspace/terraform/scenarios -it -v $HOME/.lacework.toml:/root/.lacework.toml deploy-gcp:latest \
+&& docker exec -it deploy-gcp /bin/bash
