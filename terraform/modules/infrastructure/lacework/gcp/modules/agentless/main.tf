b8af6511f67eee58495f3f0c0519c9ad57f6e1f1
credibleforce <jamiefrasermcmurray@gmail.com>
2023-03-03 08:39:56 -0800

    update gcp org-level agentless

--- terraform/modules/infrastructure/lacework/gcp/modules/agentless/main.tf
+++ terraform/modules/infrastructure/lacework/gcp/modules/agentless/main.tf
@@ -19,22 +19,22 @@ module "lacework_gcp_agentless_scanning_org_multi_region" {
   source  = "lacework/agentless-scanning/gcp"
   version = "~> 0.1"
 
-  project_filter_list = [
-    var.gcp_project_id
-  ]
+  # project_filter_list = [
+  #   var.gcp_project_id
+  # ]
 
   integration_type = "ORGANIZATION"
-  organization_id  = "data.google_project.project.org_id"
+  organization_id  = data.google_project.project.org_id
 
   global                    = true
   regional                  = true
   lacework_integration_name = "agentless_from_terraform"
 }
 
-module "lacework_gcp_agentless_scanning_org_multi_region_usc1" {
-  source  = "lacework/agentless-scanning/gcp"
-  version = "~> 0.1"
+# module "lacework_gcp_agentless_scanning_org_multi_region_usc1" {
+#   source  = "lacework/agentless-scanning/gcp"
+#   version = "~> 0.1"
 
-  regional                = true
-  global_module_reference = module.lacework_gcp_agentless_scanning_org_multi_region
-}
+#   regional                = true
+#   global_module_reference = module.lacework_gcp_agentless_scanning_org_multi_region
+# }
