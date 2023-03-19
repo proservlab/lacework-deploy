e55004ba99f7049e3ed6ba7ee0e0e66b0897473f
credibleforce <jamiefrasermcmurray@gmail.com>
2023-03-19 10:22:35 -0700

    add demo and capture to test

---
+++ terraform/test/gcp/capture.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+asciinema rec -t lacework-deploy-gcp  -i 2.5 acework-deploy-gcp.cast
+
+# gif convert
+# agg --theme monokai --font-size 20 --speed 2 --cols 120 --rows 40 --fps-cap 8 lacework-deploy-gcp.cast gcp-demo.gif
