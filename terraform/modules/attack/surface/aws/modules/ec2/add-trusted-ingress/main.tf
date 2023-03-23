9d9970a71a3268548baac0e5261e51fbb94a34ba
credibleforce <jamiefrasermcmurray@gmail.com>
2023-03-23 12:56:45 -0700

    update security groups

--- terraform/modules/attack/surface/aws/modules/ec2/add-trusted-ingress/main.tf
+++ terraform/modules/attack/surface/aws/modules/ec2/add-trusted-ingress/main.tf
@@ -6,18 +6,13 @@ resource "aws_security_group_rule" "attacker_ingress_rules" {
   cidr_blocks       = flatten([
     var.trusted_attacker_source,
     var.trusted_target_source,
-    var.trusted_workstation_source
+    var.trusted_workstation_source,
+    var.additional_trusted_sources
   ])
   description       = "Allow all tcp inbound from workstation, attacker and target public ips"
   security_group_id = var.security_group_id
-}
 
-resource "aws_security_group_rule" "additional_ingress_rules" {
-  type              = "ingress"
-  from_port         = var.trusted_tcp_ports.from_port
-  to_port           = var.trusted_tcp_ports.to_port
-  protocol          = "tcp"
-  cidr_blocks       = var.additional_trusted_sources
-  description       = "Allow all tcp inbound from additional sources public ips"
-  security_group_id = var.security_group_id
+  timeouts {
+    create = "10m"
+  }
 }
