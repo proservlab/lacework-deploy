117f580624a9def74b55567267e1e19f5fc4f4f6
credibleforce <jamiefrasermcmurray@gmail.com>
2023-03-30 09:43:07 -0700

    update base

--- terraform/modules/attack/surface/kubernetes/aws/vulnerable/rdsapp/app/app.py
+++ terraform/modules/attack/surface/kubernetes/aws/vulnerable/rdsapp/app/app.py
@@ -1,10 +1,59 @@
 # coding:utf8
+# much fun: https://github.com/u17zl/SSTI-flask-session-forge
 
 import uuid
 from flask import Flask, request, make_response, session,render_template, url_for, redirect, render_template_string
+from flask_api import status
+import os
+import pymysql
+from pymysql.err import DatabaseError
+import json
+import sys
+import boto3
+
+DB_APP_URL = os.environ.get("DB_SERVICE_URL")
+DB_USER_NAME = os.environ.get("DB_USER_NAME")
+DB_NAME = os.environ.get("DB_NAME")
+DB_PORT = os.environ.get("DB_PORT")
+DB_REGION = os.environ.get("DB_REGION")
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='Hello World!'
+
+# use provided cluster pod iam role to get db token
+client = boto3.client('rds')
+
+app.logger.info('DB_APP_URL is ' + str(DB_APP_URL))
+app.logger.info('DB_USER_NAME is ' + str(DB_USER_NAME))
+app.logger.info('DB_NAME is ' + str(DB_NAME))
+app.logger.info('DB_PORT is ' + str(DB_PORT))
+app.logger.info('DB_REGION is ' + str(DB_REGION))
+
+os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
+
+SSL_CA='rds-combined-ca-bundle.pem'
+
+app.logger.info(SSL_CA)
+
+#gets the credentials from .aws/credentials
+client = boto3.client('rds')
+
+# Connect to the database
+def create_connection():
+    # Construct SSL
+    ssl = {'ca': 'rds-combined-ca-bundle.pem'}
+    token = client.generate_db_auth_token(DBHostname=DB_APP_URL, Port=3306, DBUsername=DB_USER_NAME, Region=DB_REGION)
+    app.logger.info('token ' + token)
+    return pymysql.connect(host=DB_APP_URL,
+                             user=DB_USER_NAME,
+                             password=token,
+                             port=3306,
+                             db=DB_NAME,
+                             ssl=ssl,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor
+    )
+
 
 @app.route('/')
 def index():
@@ -41,6 +90,41 @@ def login():
     else:
         return "login failed"
 
+@app.route('/catalog')
+def catalog():
+    app.logger.info('Inside Get request')
+    try:
+        connection = create_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT `prodId`, `prodName` FROM `product`")
+
+        payload = []
+        content = {}
+        list_of_names = {}
+        for row in cursor.fetchall():
+            prodId = str(row["prodId"])
+            prodName = str(row["prodName"])
+            list_of_names[prodId] = prodName
+            content = {row['prodId']:row['prodName']}
+            payload.append(content)
+        app.logger.info(list_of_names)
+        cursor.close()
+        connection.close()  
+        return {
+            "products": list_of_names
+        }
+    except DatabaseError as e:
+        err_code = e.args[0]
+        if err_code == 2003:
+            app.logger.error('bad connection string')
+        return 'Error: Bad connection string', status.HTTP_500_INTERNAL_SERVER_ERROR
+    except KeyError as e:
+        app.logger.error('Error 500 Could not retrieve information ' + e.__doc__ )
+        return 'Error: Could not retrieve information', status.HTTP_500_INTERNAL_SERVER_ERROR
+    except Exception as e:
+        app.logger.error('Error 400 Could not retrieve information ' + e.__doc__ )
+        return 'Error: Could not retrieve information', status.HTTP_400_BAD_REQUEST
+
 @app.errorhandler(404)
 def page_not_found(e):
     template='''
@@ -51,7 +135,7 @@ def page_not_found(e):
         </div>
         {%% endblock %%}
     '''%(request.url)
-    return render_template_string(template),404
+    return render_template_string(template),status.HTTP_400_BAD_REQUEST
 
 @app.route("/logout")
 def logout():
