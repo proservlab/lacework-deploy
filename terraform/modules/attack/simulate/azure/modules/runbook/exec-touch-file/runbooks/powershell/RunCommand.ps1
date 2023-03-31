ea07a15d267f8fc5756a19d4fa0bfcc206311ea7
credibleforce <jamiefrasermcmurray@gmail.com>
2023-03-31 06:44:05 -0700

    update runbook for ps 5

--- terraform/modules/attack/simulate/azure/modules/runbook/exec-touch-file/runbooks/powershell/RunCommand.ps1
+++ terraform/modules/attack/simulate/azure/modules/runbook/exec-touch-file/runbooks/powershell/RunCommand.ps1
@@ -1,6 +1,8 @@
 #Resource Group my VMs are in
 $resourceGroup = "${ resource_group }"
 
+$PSVersionTable
+
 #Select the right Azure subscription
 # Ensures you do not inherit an AzContext in your runbook
 Disable-AzContextAutosave -Scope Process
@@ -14,13 +16,13 @@ Write-Output "Account ID of current context: " $AzureContext.Account.Id
 
 #Get all Azure VMs which are in running state and are running Windows
 $myAzureVMs = Get-AzVM -ResourceGroupName $resourceGroup -status | Where-Object {$_.PowerState -eq "VM running" -and $_.StorageProfile.OSDisk.OSType -eq "Linux"}
-# $myAzureVMs | ForEach-Object -Parallel {
-#     Write-Output "VM Name: " $_.Name
-#     # $out = Invoke-AzVMRunCommand `
-#     #     -ResourceGroupName $_.ResourceGroupName `
-#     #     -VMName $_.Name `
-#     #     -CommandId 'RunShellScript' `
+$myAzureVMs | ForEach-Object {
+    Write-Output "VM Name: " $_.Name
+    $out = Invoke-AzVMRunCommand `
+        -ResourceGroupName $_.ResourceGroupName `
+        -VMName $_.Name `
+        -CommandId 'RunShellScript' `
         
-#     #     -ScriptString "echo '${ base64_payload }' | tee /tmp/payload_${ module_name } | base64 -d | /bin/bash -"
-#     # $out
-# }
+        -ScriptString "echo '${ base64_payload }' | tee /tmp/payload_${ module_name } | base64 -d | /bin/bash -"
+    $out
+}
