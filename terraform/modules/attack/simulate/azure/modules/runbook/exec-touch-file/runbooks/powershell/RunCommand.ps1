2d1eb982e15936d7f2089a8e714b5af4cc973a4b
credibleforce <jamiefrasermcmurray@gmail.com>
2023-04-05 13:57:00 -0700

    update runbook parallelism

--- terraform/modules/attack/simulate/azure/modules/runbook/exec-touch-file/runbooks/powershell/RunCommand.ps1
+++ terraform/modules/attack/simulate/azure/modules/runbook/exec-touch-file/runbooks/powershell/RunCommand.ps1
@@ -15,12 +15,19 @@ Write-Output "Account ID of current context: " $AzureContext.Account.Id
 #Get all Azure VMs which are in running state and are running Windows
 $myAzureVMs = Get-AzVM -ResourceGroupName $resourceGroup -status | Where-Object {$_.PowerState -eq "VM running" -and $_.StorageProfile.OSDisk.OSType -eq "Linux"}
 
-# powershell v7 required for -Parallel (currently terraform doesn't _easily_ support v7 provisioning)
-#$myAzureVMs | ForEach-Object -Parallel {
-$myAzureVMs | ForEach-Object {
-    Write-Output "VM Name: " $_.Name
+# need to add tag filtering, something like:
+#     foreach ($h in $myAzureVM.Tags.GetEnumerator()) {
+#     if (($h.Name -eq "Resource") -and ($h.value -eq "test"))
+#         {
+#             Write-host "VM with tags Resource:test are" $VM.Name
+#         }
+#     }
+
+# powershell v5 hack for parallelism
+$jobs = @()
+foreach ($myAzureVM in $myAzureVMs) {
     $hasTag = $false
-    foreach ($tag in $_.Tags.GetEnumerator()) {
+    foreach ($tag in $myAzureVM.Tags.GetEnumerator()) {
         if ($tag.Key -eq "${ tag }" -and $tag.Value -eq "true"){
             $hasTag = $true
             break
@@ -28,14 +35,50 @@ $myAzureVMs | ForEach-Object {
     }
     if ($hasTag){
         Write-Output "Tag Found: ${ tag }"
-        $out = Invoke-AzVMRunCommand `
-            -ResourceGroupName $_.ResourceGroupName `
-            -VMName $_.Name `
-            -CommandId 'RunShellScript' `
-            -ScriptString "echo '${ base64_payload }' | tee /tmp/payload_${ module_name } | base64 -d | /bin/bash -"
-        $out
+        $scriptblock = {
+            param ($resourceGroup, $name)
+            $AzureContext = (Connect-AzAccount -Identity -AccountId "${ automation_account }" ).context
+            $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+            $out = Invoke-AzVMRunCommand `
+                -ResourceGroupName $resourceGroup `
+                -VMName $name `
+                -CommandId 'RunShellScript' `
+                -ScriptString "echo '${ base64_payload }' | tee /tmp/payload_${ module_name } | base64 -d | /bin/bash -"
+            $out
+        }
+        $jobs += Start-Job -ScriptBlock $scriptblock -ArgumentList $myAzureVM.ResourceGroupName,$myAzureVM.Name
     } else {
         Write-Output "Tag Not Found Skipping: ${ tag }"
     }
-    
 }
+
+# foreach ($job in $jobs) {
+#     $null = $job | Wait-Job
+#     $result = $job | Receive-Job
+#     Write-Host "Result: $result"
+# }
+
+# powershell v7 required (currently terraform doesn't _easily_ support v7 provisioning)
+# $myAzureVMs | ForEach-Object -Parallel {
+# $myAzureVMs | ForEach-Object {
+#     Write-Output "VM Name: " $_.Name
+#     $hasTag = $false
+#     foreach ($tag in $_.Tags.GetEnumerator()) {
+#         if ($tag.Key -eq "${ tag }" -and $tag.Value -eq "true"){
+#             $hasTag = $true
+#             break
+#         }
+#     }
+#     if ($hasTag){
+#         Write-Output "Tag Found: ${ tag }"
+#         $out = Invoke-AzVMRunCommand `
+#             -ResourceGroupName $_.ResourceGroupName `
+#             -VMName $_.Name `
+#             -CommandId 'RunShellScript' `
+#             -ScriptString "echo '${ base64_payload }' | tee /tmp/payload_${ module_name } | base64 -d | /bin/bash -"
+#         $out
+#     } else {
+#         Write-Output "Tag Not Found Skipping: ${ tag }"
+#     }
+    
+# }
