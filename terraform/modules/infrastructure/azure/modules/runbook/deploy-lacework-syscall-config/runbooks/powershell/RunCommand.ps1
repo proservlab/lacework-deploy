c51c7cdcdb8a5cb322b2d7b10fc1b39285aea8eb
credibleforce <jamiefrasermcmurray@gmail.com>
2023-05-01 08:16:10 -0700

    simplify runcommand.ps1

--- terraform/modules/infrastructure/azure/modules/runbook/deploy-lacework-syscall-config/runbooks/powershell/RunCommand.ps1
+++ terraform/modules/infrastructure/azure/modules/runbook/deploy-lacework-syscall-config/runbooks/powershell/RunCommand.ps1
@@ -1,7 +1,7 @@
+
 #Resource Group my VMs are in
 $resourceGroup = "${ resource_group }"
 
-#Select the right Azure subscription
 # Ensures you do not inherit an AzContext in your runbook
 Disable-AzContextAutosave -Scope Process
 
@@ -13,48 +13,16 @@ $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -Defa
 Write-Output "Account ID of current context: " $AzureContext.Account.Id
 
 #Get all Azure VMs which are in running state and are running Windows
-$myAzureVMs = Get-AzVM -ResourceGroupName $resourceGroup -status | Where-Object {$_.PowerState -eq "VM running" -and $_.StorageProfile.OSDisk.OSType -eq "Linux"}
-
-# powershell v5 hack for parallelism
-$jobs = @()
-foreach ($myAzureVM in $myAzureVMs) {
-    Write-Output "VM Name: " $myAzureVM.Name
-    $hasTag = $false
-    foreach ($tag in $myAzureVM.Tags.GetEnumerator()) {
-        if ($tag.Key -eq "${ tag }" -and $tag.Value -eq "true"){
-            $hasTag = $true
-            break
-        }
-    }
-    if ($hasTag){
-        Write-Output "Tag Found: ${ tag }"
-        $scriptblock = {
-            param ($subscriptionName, $resourceGroup, $name)
-            $rnd = Get-Random -Minimum 300 -Maximum 600
-            Write-Output "Sleeping for $rnd seconds..."
-            Start-Sleep -Seconds $rnd
-            Write-Output "Starting Execution: $resourceGroup"
-            $success = $false
-            Import-Module Az.Accounts -ErrorAction Stop
-            $success = $true
-            Write-Output "Successfully imported Az.Accounts"
-            $AzureContext = (Connect-AzAccount -Identity -AccountId "${ automation_account }" ).context
-            $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
-            $out = Invoke-AzVMRunCommand `
+$Jobs = @{}
+Get-AzVM -ResourceGroupName $resourceGroup -status | Where-Object { `
+    $_.PowerState -eq "VM running" `
+    -and $_.StorageProfile.OSDisk.OSType -eq "Linux" `
+    -and $_.Tags.Keys -contains "${ tag }" `
+    -and $_.Tags["${ tag }"] -eq "true" `
+} | ForEach-Object {
+    Invoke-AzVMRunCommand `
                 -ResourceGroupName $resourceGroup `
-                -VMName $name `
+                -VMName $_.name `
                 -CommandId 'RunShellScript' `
                 -ScriptString "echo '${ base64_payload }' | tee /tmp/payload_${ module_name } | base64 -d | /bin/bash - &"
-            Write-Output $out.Value[0].Message
-            Write-Output "Done."
-        }
-        $jobs += Start-Job -ScriptBlock $scriptblock -ArgumentList $AzureContext.Subscription.Name,$myAzureVM.ResourceGroupName,$myAzureVM.Name
-    } else {
-        Write-Output "Tag Not Found Skipping: ${ tag }"
-    }
 }
-Write-Output "Started all jobs. Receiving results."
-$jobs | % { $_ | Wait-Job }
-$jobs | % { $json = $($_ | Select-Object *  | ConvertTo-Json); Write-Output "Result: $json" }
-
-Write-Output "Done."
