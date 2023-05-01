e3e7c30aeefe66f583443ccd1a77ecf62ea1b29f
credibleforce <jamiefrasermcmurray@gmail.com>
2023-05-01 07:57:31 -0700

    update

--- requires_migration/azure/modules/compute/versions.tf
+++ requires_migration/azure/modules/compute/versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azuread = {
       source = "hashicorp/azuread"
-      version = "~> 2.28"
+      version = "~> 2.38.0"
     }
 
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 2.28"
+      version = "~> 3.54.0"
     }
   }
 }
