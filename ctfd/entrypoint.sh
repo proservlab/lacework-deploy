c2e13cdebb2e158cc964f220d702e75c054ab2c8
credibleforce <jamiefrasermcmurray@gmail.com>
2023-05-08 07:47:45 -0700

    update

--- ctfd/entrypoint.sh
+++ ctfd/entrypoint.sh
@@ -3,18 +3,18 @@
 # Run the original entrypoint command from the CTFd Docker image
 /opt/CTFd/docker-entrypoint.sh "$@" &
 
-# Wait for the CTFd web server to be available
-until python -c "import requests; requests.get('http://localhost:8000/setup')" 2>/dev/null; do
-  echo "Waiting for CTFd web server to be available..."
-  sleep 5
-done
-echo "CTFd web server is available."
+# # Wait for the CTFd web server to be available
+# until python -c "import requests; requests.get('http://localhost:8000/setup')" 2>/dev/null; do
+#   echo "Waiting for CTFd web server to be available..."
+#   sleep 5
+# done
+# echo "CTFd web server is available."
 
-# Create admin user
-/opt/CTFd/CTFd/cli/users.py create --name admin --email admin@example.com --password mysecurepassword --type admin
-
-# Generate and save API token
-/bin/sh -c "echo 'from CTFd.models import Users; print(Users.query.filter_by(email=\"admin@example.com\").first().get_api_token())' | flask shell" > /api_tokens/admin_token.txt
+# # Create admin user
+# /opt/CTFd/CTFd/cli/users.py create --name admin --email admin@example.com --password mysecurepassword --type admin
+# 8ffe2a73987138c6d3e93e008dfa84b16792d218bda1be84f01618f1a65833a4
+# # Generate and save API token
+# /bin/sh -c "echo 'from CTFd.models import Users; print(Users.query.filter_by(email=\"admin@example.com\").first().get_api_token())' | flask shell" > /api_tokens/admin_token.txt
 
 # Keep the container running
-tail -f /dev/null
+wait
