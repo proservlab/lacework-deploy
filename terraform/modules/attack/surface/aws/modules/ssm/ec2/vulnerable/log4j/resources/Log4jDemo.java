4208a2bc742d8a713e1e9a866177e4500684a1f5
credibleforce <jamiefrasermcmurray@gmail.com>
2023-05-10 06:24:15 -0700

    update

--- terraform/modules/attack/surface/aws/modules/ssm/ec2/vulnerable/log4j/resources/Log4jDemo.java
+++ terraform/modules/attack/surface/aws/modules/ssm/ec2/vulnerable/log4j/resources/Log4jDemo.java
@@ -5,7 +5,9 @@ public class Log4jDemo {
     private static final Logger logger = LogManager.getLogger(Log4jDemo.class);
 
     public static void main(String[] args) {
+        System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase", "true");
         String userMessage = args.length > 0 ? args[0] : "Hello, World!";
         logger.info("User message: {}", userMessage);
+        logger.error("Error message: {}", userMessage);
     }
 }
