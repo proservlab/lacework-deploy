4af04724b170635f029907bad8241a3e5b50fe43
credibleforce <jamiefrasermcmurray@gmail.com>
2023-06-13 06:41:38 -0700

    update

--- terraform/modules/infrastructure/lacework/gcp/modules/audit-config/main.tf
+++ terraform/modules/infrastructure/lacework/gcp/modules/audit-config/main.tf
@@ -1,15 +1,24 @@
 data "google_project" "project" {}
 
-module "gcp_organization_audit_log" {
-  source  = "lacework/audit-log/gcp"
-  version = "~> 3.4"
 
-  bucket_force_destroy         = true
-  org_integration              = true
-  use_existing_service_account = false
-  organization_id              = data.google_project.project.org_id
-  project_id                   = var.gcp_project_id
+module "gcp_organization_level_pub_sub_audit_log" {
+  source           = "lacework/pub-sub-audit-log/gcp"
+  version          = "~> 0.2.2"
+  integration_type = "ORGANIZATION"
+  project_id       = var.gcp_project_id
+  organization_id  = data.google_project.project.org_id
 }
+
+# module "gcp_organization_audit_log" {
+#   source  = "lacework/audit-log/gcp"
+#   version = "~> 3.4"
+
+#   bucket_force_destroy         = true
+#   org_integration              = true
+#   use_existing_service_account = false
+#   organization_id              = data.google_project.project.org_id
+#   project_id                   = var.gcp_project_id
+# }
 
 module "gcp_organization_config" {
   source  = "lacework/config/gcp"
