9f2cdeebf34eac8a082be0d50c16a9dc773eb65e
credibleforce <jamiefrasermcmurray@gmail.com>
2023-06-22 12:32:45 -0700

    update

--- terraform/modules/infrastructure/aws/modules/rds/s3.tf
+++ terraform/modules/infrastructure/aws/modules/rds/s3.tf
@@ -92,17 +92,17 @@ data "aws_iam_policy_document" "s3_bucket_policy_ec2" {
   }
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
+resource "aws_s3_bucket_policy" "bucket_policy_rds" {
   bucket = aws_s3_bucket.bucket.id
   policy = data.aws_iam_policy_document.s3_bucket_policy_rds.json
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
+resource "aws_s3_bucket_policy" "bucket_policy_user_role" {
   bucket = aws_s3_bucket.bucket.id
-  policy = data.aws_iam_policy_document.s3_bucket_policy_user_policy.json
+  policy = data.aws_iam_policy_document.s3_bucket_policy_user_role.json
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
+resource "aws_s3_bucket_policy" "bucket_policy_ec2" {
   bucket = aws_s3_bucket.bucket.id
   policy = data.aws_iam_policy_document.s3_bucket_policy_ec2.json
 }
