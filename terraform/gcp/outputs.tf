c6b567da06fb0e2a5c74f6a881d73e464acab2de
credibleforce <jamiefrasermcmurray@gmail.com>
2023-07-04 22:43:45 -0700

    update

--- terraform/gcp/outputs.tf
+++ terraform/gcp/outputs.tf
@@ -1,9 +1,3 @@
-
-
-# output "target-compromised-credentials" {
-#   value = [for u, k in module.target-gcp-attacksurface.compromised_credentials : "${u}:${k.rendered}"]
-# }
-
 output "target-gcp-instances" {
   value = [
     for gce in can(length(module.target-gcp-infrastructure.config.context.gcp.gce)) ? module.target-gcp-infrastructure.config.context.gcp.gce : [] :
@@ -19,23 +13,17 @@ output "target-gcp-instances" {
   ]
 }
 
-# output "attacker-gcp-instances" {
-#   value = [
-#     for gce in can(length(module.attacker-gcp-infrastructure.config.context.gcp.gce)) ? module.attacker-gcp-infrastructure.config.context.gcp.gce : [] :
-#     [
-#       for compute in gce.instances : {
-#         id        = compute.instance.id
-#         name      = compute.instance.tags["Name"]
-#         public_ip = compute.instance.public_ip
-#         tags = [
-#           for k, v in compute.instance.tags : { "${k}" = v } if v != "false"
-#         ]
-#       }
-#     ]
-#   ]
-# }
-
-# output "gce" {
-#   sensitive = true
-#   value     = module.target-aws-infrastructure.config.context.gcp.gce
-# }
+output "target-gcp-instances" {
+  value = [
+    for gce in can(length(module.attacker-gcp-infrastructure.config.context.gcp.gce)) ? module.target-gcp-infrastructure.config.context.gcp.gce : [] :
+    [
+      for compute in gce.instances : {
+        id         = compute.instance.id
+        name       = compute.instance.labels.name
+        private_ip = compute.instance.network_interface.0.network_ip
+        public_ip  = compute.instance.network_interface.0.access_config.0.nat_ip
+        labels     = { for k, v in compute.instance.labels : k => v if v != "false" }
+      }
+    ]
+  ]
+}
