68d77f8266f0a347aa5e6c14979521ec6a204998
credibleforce <jamiefrasermcmurray@gmail.com>
2023-07-04 22:37:00 -0700

    update

--- terraform/gcp/outputs.tf
+++ terraform/gcp/outputs.tf
@@ -4,20 +4,20 @@
 #   value = [for u, k in module.target-gcp-attacksurface.compromised_credentials : "${u}:${k.rendered}"]
 # }
 
-# output "target-gcp-instances" {
-#   value = [
-#     for gcp in can(length(module.target-gcp-infrastructure.config.context.gcp.gce)) ? module.target-gcp-infrastructure.config.context.gcp.gce : [] :
-#     [
-#       for compute in gce.instances : {
-#         id         = compute.instance.id
-#         name       = compute.instance.tags["Name"]
-#         private_ip = compute.instance.private_ip
-#         public_ip  = compute.instance.public_ip
-#         tags       = { for k, v in compute.instance.tags : k => v if v != "false" }
-#       }
-#     ]
-#   ]
-# }
+output "target-gcp-instances" {
+  value = [
+    for gce in can(length(module.target-gcp-infrastructure.config.context.gcp.gce)) ? module.target-gcp-infrastructure.config.context.gcp.gce : [] :
+    [
+      for compute in gce.instances : {
+        id         = compute.instance.id
+        name       = compute.instance.instance_id
+        private_ip = compute.instance.network_interface.0.network_ip
+        public_ip  = compute.instance.network_interface.0.access_config.0.nat_ip
+        labels     = compute.instance.labels
+      }
+    ]
+  ]
+}
 
 # output "attacker-gcp-instances" {
 #   value = [
