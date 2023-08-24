12e549d2b628cb2f6820a6ce0cf6f4bc895b9e6f
credibleforce <jamiefrasermcmurray@gmail.com>
2023-08-24 10:57:40 -0700

    update

--- terraform/modules/infrastructure/gcp/modules/gce/instance/main.tf
+++ terraform/modules/infrastructure/gcp/modules/gce/instance/main.tf
@@ -152,6 +152,12 @@ resource "google_compute_instance" "instance" {
     service_account {
         # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
         email  = local.service_account_email
-        scopes = ["cloud-platform"]
+        scopes = ["https://www.googleapis.com/auth/servicecontrol",
+                  "https://www.googleapis.com/auth/service.management.readonly",
+                  "https://www.googleapis.com/auth/logging.write",
+                  "https://www.googleapis.com/auth/monitoring.write",
+                  "https://www.googleapis.com/auth/trace.append",
+                  "https://www.googleapis.com/auth/devstorage.read_only",
+                  "https://www.googleapis.com/auth/cloud.useraccounts.readonly"]
     }
 }
