f5267c87a857b1b0de64a6949316eda767a8e6c6
credibleforce <jamiefrasermcmurray@gmail.com>
2023-10-26 20:41:26 -0700

    update

--- terraform/modules/attack/simulate/aws/modules/ssm/responder-reverse-shell-multistage/resources/pwncat.sh
+++ terraform/modules/attack/simulate/aws/modules/ssm/responder-reverse-shell-multistage/resources/pwncat.sh
@@ -3,7 +3,7 @@ function log {
     echo `date -u +"%Y-%m-%dT%H:%M:%SZ"`" $1"
     echo `date -u +"%Y-%m-%dT%H:%M:%SZ"`" $1" >> $LOGFILE
 }
-MAXLOG=2
+MAXLOG=4
 for i in `seq $((MAXLOG-1)) -1 1`; do mv "$LOGFILE."{$i,$((i+1))} 2>/dev/null || true; done
 mv $LOGFILE "$LOGFILE.1" 2>/dev/null || true
 check_apt() {
@@ -20,5 +20,6 @@ log "starting background process via screen..."
 screen -S pwncat -X quit
 nohup /bin/bash -c "screen -d -L -Logfile $PWNCAT_LOG -S pwncat -m python3.9 listener.py --port ${listen_port}" >/dev/null 2>&1 &
 screen -S pwncat -X colon "logfile flush 0^M"
+
 log "listener started."
 log "done"
