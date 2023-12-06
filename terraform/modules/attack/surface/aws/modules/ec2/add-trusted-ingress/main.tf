8e11e69cb6e96675026659d4f5064e2fb2fda5c0
credibleforce <jamiefrasermcmurray@gmail.com>
2023-12-06 14:51:17 -0800

    sort aws security group rules to avoid redeploy

--- terraform/modules/attack/surface/aws/modules/ec2/add-trusted-ingress/main.tf
+++ terraform/modules/attack/surface/aws/modules/ec2/add-trusted-ingress/main.tf
@@ -3,12 +3,12 @@ resource "aws_security_group_rule" "attacker_ingress_rules" {
   from_port         = var.trusted_tcp_ports.from_port
   to_port           = var.trusted_tcp_ports.to_port
   protocol          = "tcp"
-  cidr_blocks       = flatten([
+  cidr_blocks       = sort(flatten([
     var.trusted_attacker_source,
     var.trusted_target_source,
     var.trusted_workstation_source,
     var.additional_trusted_sources
-  ])
+  ]))
   description       = "Allow all tcp inbound from workstation, attacker and target public ips"
   security_group_id = var.security_group_id
 
