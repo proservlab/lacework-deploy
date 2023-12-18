b7b430b7b4cc5252f2a5bd2e67f6fa314f8e716a
credibleforce <jamiefrasermcmurray@gmail.com>
2023-12-18 12:55:47 -0800

    update lacework-deploy

--- terraform/modules/infrastructure/aws/modules/rds/s3.tf
+++ terraform/modules/infrastructure/aws/modules/rds/s3.tf
@@ -7,11 +7,6 @@ resource "aws_s3_bucket" "bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-  bucket = aws_s3_bucket.example.id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-}
-
 ######################################################
 # Set S3 bucket policy to allow the db roles read access
 ######################################################
