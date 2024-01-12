0a705428224d1bc3adc751ee213be092c124877a
credibleforce <jamiefrasermcmurray@gmail.com>
2024-01-12 08:14:20 -0800

    update payload input

--- terraform/modules/common/any/payload/linux/modules/ssh-user/main.tf
+++ terraform/modules/common/any/payload/linux/modules/ssh-user/main.tf
@@ -19,18 +19,18 @@ locals {
     service ssh reload
     log "Done."
     EOT
-    base64_payload = base64encode(templatefile("../../delayed_start.sh", {
-        script_name = basename(path.module),
-        log_rotation_count = 2,
-        apt_pre_tasks = "",
-        apt_packages = "",
-        apt_post_tasks = "",
-        yum_pre_tasks =  "",
-        yum_packages = "",
-        yum_post_tasks = "",
-        script_delay_secs = 30,
+    base64_payload = base64encode(templatefile("${path.root}/modules/common/any/payload/linux/delayed_start.sh", { config = {
+        script_name = basename(path.module)
+        log_rotation_count = 2
+        apt_pre_tasks = ""
+        apt_packages = ""
+        apt_post_tasks = ""
+        yum_pre_tasks =  ""
+        yum_packages = ""
+        yum_post_tasks = ""
+        script_delay_secs = 30
         next_stage_payload = local.payload
-    }))
+    }}))
 
     outputs = {
         base64_payload = local.base64_payload
