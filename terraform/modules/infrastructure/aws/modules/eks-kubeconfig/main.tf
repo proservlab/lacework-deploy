2a2f41b3bd7c08575f8f90c135b217d88535a188
credibleforce <jamiefrasermcmurray@gmail.com>
2024-01-24 09:31:05 -0800

    fix kubeconfig missing aws profile on aws eks update-kubeconfig

--- terraform/modules/infrastructure/aws/modules/eks-kubeconfig/main.tf
+++ terraform/modules/infrastructure/aws/modules/eks-kubeconfig/main.tf
@@ -10,8 +10,16 @@ resource "null_resource" "eks_context_switcher" {
                 set -e
                 echo 'Applying Auth ConfigMap with kubectl...'
                 aws eks wait cluster-active --profile '${var.aws_profile_name}' --name '${var.cluster_name}'
+                if ! command -v yq; then
+                  wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq &&\
+                  chmod +x /usr/local/bin/yq
+                fi
                 aws eks update-kubeconfig --profile '${var.aws_profile_name}' --name '${var.cluster_name}' --region=${var.region}
                 aws eks update-kubeconfig --profile '${var.aws_profile_name}' --name '${var.cluster_name}' --region=${var.region} --kubeconfig=${var.kubeconfig_path}
+                yq -i -r '(.users[] | select(.name == "arn:aws:eks:us-east-1:421996542948:cluster/dev-target-f18eaefa")|.user.exec.env[0].name) = "AWS_PROFILE"' -i ~/.kube/config
+                yq -i -r '(.users[] | select(.name == "arn:aws:eks:us-east-1:421996542948:cluster/dev-target-f18eaefa")|.user.exec.env[0].value) = "${var.aws_profile_name}"' -i ~/.kube/config
+                yq -i -r '(.users[] | select(.name == "arn:aws:eks:us-east-1:421996542948:cluster/dev-target-f18eaefa")|.user.exec.env[0].name) = "AWS_PROFILE"' -i ${var.kubeconfig_path}
+                yq -i -r '(.users[] | select(.name == "arn:aws:eks:us-east-1:421996542948:cluster/dev-target-f18eaefa")|.user.exec.env[0].value) = "${var.aws_profile_name}"' -i ${var.kubeconfig_path}
               EOT
   }
 }
