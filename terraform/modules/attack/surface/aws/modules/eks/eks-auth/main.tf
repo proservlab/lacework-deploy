e0b077586934cc868f0f35ded8ab9a51982ff9b1
credibleforce <jamiefrasermcmurray@gmail.com>
2024-01-23 20:15:59 -0800

    update eks-auth to include assumed role user

--- terraform/modules/attack/surface/aws/modules/eks/eks-auth/main.tf
+++ terraform/modules/attack/surface/aws/modules/eks/eks-auth/main.tf
@@ -3,8 +3,17 @@ data "aws_caller_identity" "current" {}
 locals {
     cluster_name = "${var.cluster_name}-${var.environment}-${var.deployment}"
     aws_account_id = data.aws_caller_identity.current.account_id
+    use_assumed_role = can(regex(".*:assumed-role/(.*)/", data.aws_caller_identity.current.arn))
+    current_user_arn       = local.use_assumed_role ? "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${regex(".*:assumed-role/(.*)/", data.aws_caller_identity.current.arn)[0]}" : data.aws_caller_identity.current.arn
     read_role_name = "read-pods"
     admin_role_name = "admin-pods"
+
+
+    current_user_admins = use_assumed_role ? [] : [ current_user_arn ]
+    current_role_admins = use_assumed_role ? [ current_user_arn ] : []
+
+    iam_eks_admins = var.iam_eks_admins
+    iam_eks_readers = var.iam_eks_readers
 }
 
 data "aws_iam_user" "read_users" {
@@ -31,6 +40,18 @@ resource "kubernetes_config_map_v1_data" "aws_auth_configmap" {
                         - system:nodes
                     YAML
         mapUsers =  <<-YAML
+                    %{ for user_arn in current_role_admins }
+                    - groups:
+                        - ${ local.admin_role_name }
+                      rolearn: ${ user_arn }
+                      username: ${ regex(".*:assumed-role/(.*)/", data.aws_caller_identity.current.arn)[0] }
+                    %{ endfor }
+                    %{ for user_arn in current_user_admins }
+                    - groups:
+                        - ${ local.admin_role_name }
+                      userarn: ${ user_arn }
+                      username: ${ reverse(split("/", user_arn))[0] }
+                    %{ endfor }
                     %{ for iam_user in data.aws_iam_user.read_users }
                     - groups:
                         - ${ local.read_role_name }
