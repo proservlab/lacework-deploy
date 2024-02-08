ff52af3d75ae622b13af3b1c7169863614b044ae
credibleforce <jamiefrasermcmurray@gmail.com>
2024-02-08 09:08:10 -0800

    update reloader tag

--- terraform/modules/attack/surface/kubernetes/aws/authapp/main.tf
+++ terraform/modules/attack/surface/kubernetes/aws/authapp/main.tf
@@ -11,41 +11,47 @@ module "id" {
 ##################################################
 
 locals {
-    app_name = var.app
-    app_namespace = var.app_namespace
-    service_account = "authapp"
+  app_name        = var.app
+  app_namespace   = var.app_namespace
+  service_account = "authapp"
 }
 
 module "deployment" {
-  source        = "../../common/terraform-kubernetes-deployment-master"
-  namespace     = local.app_namespace
-  image         = "${aws_ecr_repository.repo.repository_url}:${data.external.hash.result["hash"]}"
-  name          = local.app_name
-  command       = var.command
-  args          = var.args
-  
+  source    = "../../common/terraform-kubernetes-deployment-master"
+  namespace = local.app_namespace
+  image     = "${aws_ecr_repository.repo.repository_url}:${data.external.hash.result["hash"]}"
+  name      = local.app_name
+  command   = var.command
+  args      = var.args
+
   service_account_name = kubernetes_service_account.this.metadata[0].name
-  
+
+  deployment_annotations = {
+    # configmap.reloader.stakater.com/reload: "test-configmap"
+    secret.reloader.stakater.com / reload : kubernetes_secret.this.metadata[0].name
+    # reloader.stakater.com/auto: "true"
+  }
+
   env_secret = {
-    ADMINPWD = kubernetes_secret.this.metadata[0].name 
-    USERPWD = kubernetes_secret.this.metadata[0].name 
+    ADMINPWD = kubernetes_secret.this.metadata[0].name
+    USERPWD  = kubernetes_secret.this.metadata[0].name
   }
 
   internal_port = [{
-    name = "container"
+    name          = "container"
     internal_port = var.container_port
   }]
   security_context_container = [{
-      privileged = var.privileged
+    privileged = var.privileged
   }]
   custom_labels = {
     app = local.app_name
   }
   template_annotations = {
     app = local.app_name
-    "reloader.stakater.com/auto": "true"
+    "reloader.stakater.com/auto" : "true"
   }
-  replicas      = 1
+  replicas = 1
 
   depends_on = [
     kubernetes_namespace.this
--- terraform/modules/attack/surface/kubernetes/aws/s3app/main.tf
+++ terraform/modules/attack/surface/kubernetes/aws/s3app/main.tf
@@ -11,11 +11,11 @@ module "id" {
 ##################################################
 
 locals {
-  app_name = var.app
+  app_name      = var.app
   app_namespace = var.app_namespace
 
   service_account_db_user = var.service_account_db_user
-  service_account = var.service_account
+  service_account         = var.service_account
 }
 
 data "aws_caller_identity" "current" {}
@@ -23,37 +23,43 @@ data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
 
 module "deployment" {
-    source        = "../../common/terraform-kubernetes-deployment-master"
-    namespace     = local.app_namespace
-    image         = "${aws_ecr_repository.repo.repository_url}:${data.external.hash.result["hash"]}"
-    name          = local.app_name
-    command       = var.command
-    args          = var.args
-    service_account_name = kubernetes_service_account.web_service.metadata[0].name
-    
-    env_secret = {
-        ADMINPWD = kubernetes_secret.this.metadata[0].name 
-        USERPWD = kubernetes_secret.this.metadata[0].name 
-        BUCKET_NAME = kubernetes_secret.this.metadata[0].name 
-    }
-    
-    internal_port = [{
-        name = "container"
-        internal_port = var.container_port
-    }]
-    security_context_container = [{
-        privileged = var.privileged
-    }]
-    custom_labels = {
-        app = local.app_name
-    }
-    template_annotations = {
-        app = local.app_name
-    }
-    replicas      = 1
+  source               = "../../common/terraform-kubernetes-deployment-master"
+  namespace            = local.app_namespace
+  image                = "${aws_ecr_repository.repo.repository_url}:${data.external.hash.result["hash"]}"
+  name                 = local.app_name
+  command              = var.command
+  args                 = var.args
+  service_account_name = kubernetes_service_account.web_service.metadata[0].name
 
-    depends_on = [
-        kubernetes_namespace.this,
-        kubernetes_service_account.web_service
-    ]
+  deployment_annotations = {
+    # configmap.reloader.stakater.com/reload: "test-configmap"
+    secret.reloader.stakater.com / reload : kubernetes_secret.this.metadata[0].name
+    # reloader.stakater.com/auto: "true"
+  }
+
+  env_secret = {
+    ADMINPWD    = kubernetes_secret.this.metadata[0].name
+    USERPWD     = kubernetes_secret.this.metadata[0].name
+    BUCKET_NAME = kubernetes_secret.this.metadata[0].name
+  }
+
+  internal_port = [{
+    name          = "container"
+    internal_port = var.container_port
+  }]
+  security_context_container = [{
+    privileged = var.privileged
+  }]
+  custom_labels = {
+    app = local.app_name
+  }
+  template_annotations = {
+    app = local.app_name
+  }
+  replicas = 1
+
+  depends_on = [
+    kubernetes_namespace.this,
+    kubernetes_service_account.web_service
+  ]
 }
