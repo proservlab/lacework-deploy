8147275bff2df62baab5fc346910d45a1cbbbee5
credibleforce <jamiefrasermcmurray@gmail.com>
2024-02-18 10:28:35 -0800

    update eks locals

--- terraform/build.sh
+++ terraform/build.sh
@@ -332,6 +332,7 @@ EOF
             echo "Add env AWS_REGION to deployment kubeconfig - Result: $?"
             yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[1].value) = strenv(ATTACKER_AWS_REGION)' -i "$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
             echo "Add env AWS_REGION value to deployment kubeconfig - Result: $?"
+            cat "$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
         done
     fi
     if [[ "$TARGET_EKS_ENABLED" == "true" ]]; then 
@@ -362,6 +363,7 @@ EOF
             echo "Add env AWS_REGION to deployment kubeconfig - Result: $?"
             yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[1].value) = strenv(TARGET_AWS_REGION)' -i "$HOME/.kube/$CSP-target-$DEPLOYMENT-kubeconfig"
             echo "Add env AWS_REGION value to deployment kubeconfig - Result: $?"
+            cat "$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
         done
     fi
 fi
