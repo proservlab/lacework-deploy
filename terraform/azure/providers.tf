c8f7c01c0ff3e90bae4e1a459ecd941fdeebf3ef
credibleforce <jamiefrasermcmurray@gmail.com>
2024-02-19 13:39:10 -0800

    add root provider for restapi

--- terraform/azure/providers.tf
+++ terraform/azure/providers.tf
@@ -2,11 +2,12 @@ provider "restapi" {
     alias = "attacker"
     uri                  = "https://api.dynu.com/v2"
     write_returns_object = true
-    debug                = true
+    debug                = false
 
     headers = {
     "API-Key" = try(var.attacker_dynu_api_key, ""),
-    "Content-Type" = "application/json"
+    "Content-Type" = "application/json",
+    "accept" = "application/json"
     }
 
     create_method  = "POST"
@@ -18,11 +19,12 @@ provider "restapi" {
     alias = "target"
     uri                  = "https://api.dynu.com/v2"
     write_returns_object = true
-    debug                = true
+    debug                = false
 
     headers = {
     "API-Key" = try(var.target_dynu_api_key, ""),
-    "Content-Type" = "application/json"
+    "Content-Type" = "application/json",
+    "accept" = "application/json"
     }
 
     create_method  = "POST"
