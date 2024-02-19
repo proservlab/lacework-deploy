3b4d9f6e0174b00ce370c147033fc70b038a340f
credibleforce <jamiefrasermcmurray@gmail.com>
2024-02-19 14:04:24 -0800

    add temp workaround

--- terraform/modules/infrastructure/dynu/dns_record/main.tf
+++ terraform/modules/infrastructure/dynu/dns_record/main.tf
@@ -2,8 +2,16 @@ locals {
   dynu_api = "https://api.dynu.com/v2"
 }
 
+data "restapi_object" "domain" {
+  path         = "/dns"
+  search_key   = "name"
+  search_value = "attacker-hub.freeddns.org"
+  id_attribute = "id"
+  results_key = "domains"
+}
+
 resource "restapi_object" "record" {
-  path          = "/dns/${var.dynu_dns_domain_id}/record"
+  path          = "/dns/${data.restapi_object.domain.id}/record"
   destroy_data  = ""
   data          = jsonencode({
     nodeName    = var.record.recordName
