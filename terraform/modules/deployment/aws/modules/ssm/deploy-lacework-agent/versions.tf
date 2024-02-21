56331ea0288678eee13646bdf005ee32e2f110dd
Credibleforce <58786780+credibleforce@users.noreply.github.com>
2024-02-21 15:22:00 -0800

    Update versions.tf

--- terraform/modules/deployment/aws/modules/ssm/deploy-lacework-agent/versions.tf
+++ terraform/modules/deployment/aws/modules/ssm/deploy-lacework-agent/versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    lacework = {
+      source  = "lacework/lacework"
+      version = "~> 1.6"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
