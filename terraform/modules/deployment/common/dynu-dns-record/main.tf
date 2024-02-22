cbe7247d37c1f7e7a860d34b40b9fc91cd4628ef
credibleforce <jamiefrasermcmurray@gmail.com>
2024-02-22 08:29:18 -0800

    update provider

--- terraform/modules/deployment/common/dynu-dns-record/main.tf
+++ terraform/modules/deployment/common/dynu-dns-record/main.tf
@@ -2,6 +2,7 @@ locals {
   dynu_api = "https://api.dynu.com/v2/dns"
   dynu_domains_response = jsondecode(data.http.dynu_domain_id.response_body)
   dynu_domain_id = one([ for domain in local.dynu_domains_response["domains"]: domain.id if domain.name == var.dynu_dns_domain  ])
+  dynu_records_response  = jsondecode(data.http.dynu_records.response_body)
 }
 
 data "http" "dynu_domain_id" {
@@ -27,9 +28,36 @@ data "http" "dynu_domain_id" {
   }
 }
 
+data "http" "dynu_records" {
+  url = "${local.dynu_api}/${local.dynu_domain_id}/record"
+  method = "GET"
+
+  # Optional request headers
+  request_headers = {
+    API-Key = var.dynu_api_key
+    Accept = "application/json"
+    Content-Type = "application/json"
+  }
+
+  retry {
+    attempts = 3
+  }
+
+  lifecycle {
+    postcondition {
+      condition     = contains([200], self.status_code) && try(length(self.response_body)>0, false)
+      error_message = "Status code invalid"
+    }
+  }
+}
+
 resource "restapi_object" "record" {
   path          = "/dns/${local.dynu_domain_id}/record"
   destroy_data  = ""
+  create_method = "POST"
+  destroy_method = "DELETE"
+  update_method = "POST"
+
   data          = var.record.recordType == "CNAME" ? jsonencode({
     nodeName    = var.record.recordName
     recordType  = var.record.recordType
@@ -52,4 +80,6 @@ resource "restapi_object" "record" {
     var.record.recordValue,
     var.record.recordType
   ]
+
+  depends_on = [ data.http.dynu_domain_id, data.http.dynu_records ]
 }
