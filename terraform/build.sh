b9046bdc28425ff41157d758cf6f0506dea8ba16
credibleforce <jamiefrasermcmurray@gmail.com>
2024-02-28 05:20:00 -0800

    add kubeconfig staging

--- terraform/build.sh
+++ terraform/build.sh
@@ -229,6 +229,8 @@ check_tf_apply(){
             infomsg "Checking for kubernetes readiness errors..."
             if grep "dial tcp \[\:\:1\]\:80\: connect\: connection refused" $LOGFILE; then
                 infomsg "Some kubernetes resources were not ready during deploy - a second apply is required..."
+                infomsg "Updating kubeconfig..."
+                stage_kubeconfig
                 echo "Running: terraform plan -parallelism=${PARALLELISM} ${NO_REFRESH_ON_PLAN} ${DESTROY} ${BACKEND} ${VARS} -out ${PLANFILE} -detailed-exitcode"
                 (
                     set -o pipefail
@@ -309,42 +311,45 @@ echo "REFRESH_ON_PLAN   = ${NO_REFRESH_ON_PLAN}"
 cd $CSP
 
 # check for sso logged out session
-if [[ "$CSP" == "aws" ]]; then
-    session_check=$(aws sts get-caller-identity ${SSO_PROFILE} 2>&1)
-    if echo $session_check | grep "The SSO session associated with this profile has expired or is otherwise invalid." > /dev/null 2>&1; then
-        read -p "> aws sso session has expired - login now? (y/n): " login
-        case "$login" in
-            y|Y )
-                aws sso login ${SSO_PROFILE}
-                ;;
-            n|N )
-                errmsg "aws session expired - manual login required."
-                exit 1
-                ;;
-            * )
-                errmsg "aws session expired - manual login required."
-                exit 1
-                ;;
-        esac
+check_session(){
+    if [[ "$CSP" == "aws" ]]; then
+        session_check=$(aws sts get-caller-identity ${SSO_PROFILE} 2>&1)
+        if echo $session_check | grep "The SSO session associated with this profile has expired or is otherwise invalid." > /dev/null 2>&1; then
+            read -p "> aws sso session has expired - login now? (y/n): " login
+            case "$login" in
+                y|Y )
+                    aws sso login ${SSO_PROFILE}
+                    ;;
+                n|N )
+                    errmsg "aws session expired - manual login required."
+                    exit 1
+                    ;;
+                * )
+                    errmsg "aws session expired - manual login required."
+                    exit 1
+                    ;;
+            esac
+        fi
     fi
-fi
+}
 
 # stage kubeconfig
-infomsg "Staging kubeconfig files..."
-CONFIG_FILES=('config' "$CSP-attacker-$DEPLOYMENT-kubeconfig" "$CSP-target-$DEPLOYMENT-kubeconfig")
-if [ ! -d "$HOME/.kube" ]; then
-    infomsg "$HOME/.kube directory not found - creating..."
-    mkdir -p "$HOME/.kube"
-fi
-# for eks we need to ensure that if kubeconfig is wiped between runs (e.g. CICD) that we repopulate as best we can
-if [[ "$CSP" == "aws" ]]; then
-    export ATTACKER_AWS_PROFILE=$(get_tfvar_value "$tfvars_file" "attacker_aws_profile")
-    export ATTACKER_AWS_REGION=$(get_tfvar_value "$tfvars_file" "attacker_aws_region")
-    export ATTACKER_EKS_ENABLED=$(cat $SCENARIOS_PATH/$WORK/attacker/infrastructure.json| jq -r 'try .context.aws.eks.enabled catch false')
-    export TARGET_AWS_PROFILE=$(get_tfvar_value "$tfvars_file" "target_aws_profile")
-    export TARGET_AWS_REGION=$(get_tfvar_value "$tfvars_file" "target_aws_region")
-    export TARGET_EKS_ENABLED=$(cat $SCENARIOS_PATH/$WORK/target/infrastructure.json| jq -r 'try .context.aws.eks.enabled catch false')
-    cat <<EOF
+stage_kubeconfig() {
+    infomsg "Staging kubeconfig files..."
+    CONFIG_FILES=('config' "$CSP-attacker-$DEPLOYMENT-kubeconfig" "$CSP-target-$DEPLOYMENT-kubeconfig")
+    if [ ! -d "$HOME/.kube" ]; then
+        infomsg "$HOME/.kube directory not found - creating..."
+        mkdir -p "$HOME/.kube"
+    fi
+    # for eks we need to ensure that if kubeconfig is wiped between runs (e.g. CICD) that we repopulate as best we can
+    if [[ "$CSP" == "aws" ]]; then
+        export ATTACKER_AWS_PROFILE=$(get_tfvar_value "$tfvars_file" "attacker_aws_profile")
+        export ATTACKER_AWS_REGION=$(get_tfvar_value "$tfvars_file" "attacker_aws_region")
+        export ATTACKER_EKS_ENABLED=$(cat $SCENARIOS_PATH/$WORK/attacker/infrastructure.json| jq -r 'try .context.aws.eks.enabled catch false')
+        export TARGET_AWS_PROFILE=$(get_tfvar_value "$tfvars_file" "target_aws_profile")
+        export TARGET_AWS_REGION=$(get_tfvar_value "$tfvars_file" "target_aws_region")
+        export TARGET_EKS_ENABLED=$(cat $SCENARIOS_PATH/$WORK/target/infrastructure.json| jq -r 'try .context.aws.eks.enabled catch false')
+        cat <<EOF
 ATTACKER_AWS_PROFILE=$ATTACKER_AWS_PROFILE
 ATTACKER_AWS_REGION=$ATTACKER_AWS_REGION
 ATTACKER_EKS_ENABLED=$ATTACKER_EKS_ENABLED
@@ -352,75 +357,82 @@ TARGET_AWS_PROFILE=$TARGET_AWS_PROFILE
 TARGET_AWS_REGION=$TARGET_AWS_REGION
 TARGET_EKS_ENABLED=$TARGET_EKS_ENABLED
 EOF
-    if [[ "$ATTACKER_EKS_ENABLED" == "true" ]]; then 
-        echo "EKS in attacker scenario enabled..."
-        if ! command -v yq; then
-            curl -LJ https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -o /usr/local/bin/yq &&\
-            chmod +x /usr/local/bin/yq
+        if [[ "$ATTACKER_EKS_ENABLED" == "true" ]]; then 
+            echo "EKS in attacker scenario enabled..."
+            if ! command -v yq; then
+                curl -LJ https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -o /usr/local/bin/yq &&\
+                chmod +x /usr/local/bin/yq
+            fi
+            echo "Found yq: $(command -v yq)"
+            CLUSTERS=$(aws eks list-clusters --profile=$ATTACKER_AWS_PROFILE --region=$ATTACKER_AWS_REGION --output=json)
+            for CLUSTER in $(echo $CLUSTERS| jq -r --arg DEPLOYMENT "$DEPLOYMENT" '.clusters[] | select(endswith((["-",$DEPLOYMENT]|join(""))))'); do
+                echo "Found cluster: $CLUSTER"
+                aws eks update-kubeconfig --profile=$ATTACKER_AWS_PROFILE --name="$CLUSTER" --region=$ATTACKER_AWS_REGION
+                aws eks update-kubeconfig --profile=$ATTACKER_AWS_PROFILE --name="$CLUSTER" --region=$ATTACKER_AWS_REGION --kubeconfig="$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
+                echo "Adding profile and region context to kubeconfig..."
+                yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[0].name) = "AWS_PROFILE"' -i "$HOME/.kube/config"
+                echo "Add env AWS_PROFILE to default kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[0].value) = strenv(ATTACKER_AWS_PROFILE)' -i "$HOME/.kube/config"
+                echo "Add env AWS_PROFILE value to default kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[1].name) = "AWS_REGION"' -i "$HOME/.kube/config"
+                echo "Add env AWS_REGION to default kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[1].value) = strenv(ATTACKER_AWS_REGION)' -i "$HOME/.kube/config"
+                echo "Add env AWS_REGION value to default kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[0].name) = "AWS_PROFILE"' -i "$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
+                echo "Add env AWS_PROFILE to deployment kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[0].value) = strenv(ATTACKER_AWS_PROFILE)' -i "$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
+                echo "Add env AWS_PROFILE value to deployment kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[1].name) = "AWS_REGION"' -i "$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
+                echo "Add env AWS_REGION to deployment kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[1].value) = strenv(ATTACKER_AWS_REGION)' -i "$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
+                echo "Add env AWS_REGION value to deployment kubeconfig - Result: $?"
+            done
         fi
-        echo "Found yq: $(command -v yq)"
-        CLUSTERS=$(aws eks list-clusters --profile=$ATTACKER_AWS_PROFILE --region=$ATTACKER_AWS_REGION --output=json)
-        for CLUSTER in $(echo $CLUSTERS| jq -r --arg DEPLOYMENT "$DEPLOYMENT" '.clusters[] | select(endswith((["-",$DEPLOYMENT]|join(""))))'); do
-            echo "Found cluster: $CLUSTER"
-            aws eks update-kubeconfig --profile=$ATTACKER_AWS_PROFILE --name="$CLUSTER" --region=$ATTACKER_AWS_REGION
-            aws eks update-kubeconfig --profile=$ATTACKER_AWS_PROFILE --name="$CLUSTER" --region=$ATTACKER_AWS_REGION --kubeconfig="$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
-            echo "Adding profile and region context to kubeconfig..."
-            yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[0].name) = "AWS_PROFILE"' -i "$HOME/.kube/config"
-            echo "Add env AWS_PROFILE to default kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[0].value) = strenv(ATTACKER_AWS_PROFILE)' -i "$HOME/.kube/config"
-            echo "Add env AWS_PROFILE value to default kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[1].name) = "AWS_REGION"' -i "$HOME/.kube/config"
-            echo "Add env AWS_REGION to default kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[1].value) = strenv(ATTACKER_AWS_REGION)' -i "$HOME/.kube/config"
-            echo "Add env AWS_REGION value to default kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[0].name) = "AWS_PROFILE"' -i "$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
-            echo "Add env AWS_PROFILE to deployment kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[0].value) = strenv(ATTACKER_AWS_PROFILE)' -i "$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
-            echo "Add env AWS_PROFILE value to deployment kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[1].name) = "AWS_REGION"' -i "$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
-            echo "Add env AWS_REGION to deployment kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(endswith("strenv(DEPLOYMENT)")|.user.exec.env[1].value) = strenv(ATTACKER_AWS_REGION)' -i "$HOME/.kube/$CSP-attacker-$DEPLOYMENT-kubeconfig"
-            echo "Add env AWS_REGION value to deployment kubeconfig - Result: $?"
-        done
-    fi
-    if [[ "$TARGET_EKS_ENABLED" == "true" ]]; then 
-        echo "EKS in target scenario enabled..."
-        if ! command -v yq; then
-            curl -LJ https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -o /usr/local/bin/yq &&\
-            chmod +x /usr/local/bin/yq
+        if [[ "$TARGET_EKS_ENABLED" == "true" ]]; then 
+            echo "EKS in target scenario enabled..."
+            if ! command -v yq; then
+                curl -LJ https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -o /usr/local/bin/yq &&\
+                chmod +x /usr/local/bin/yq
+            fi
+            echo "Found yq: $(command -v yq)"
+            CLUSTERS=$(aws eks list-clusters --profile=$TARGET_AWS_PROFILE --region=$TARGET_AWS_REGION)
+            for CLUSTER in $(echo $CLUSTERS | jq -r --arg DEPLOYMENT "$DEPLOYMENT" '.clusters[] | select(endswith((["-",$DEPLOYMENT]|join(""))))'); do
+                echo "Found cluster: $CLUSTER"
+                aws eks update-kubeconfig --profile=$TARGET_AWS_PROFILE --name="$CLUSTER" --region=$TARGET_AWS_REGION
+                aws eks update-kubeconfig --profile=$TARGET_AWS_PROFILE --name="$CLUSTER" --region=$TARGET_AWS_REGION --kubeconfig="$HOME/.kube/$CSP-target-$DEPLOYMENT-kubeconfig"
+                yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[0].name) = "AWS_PROFILE"' -i "$HOME/.kube/config"
+                echo "Add env AWS_PROFILE to default kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[0].value) = strenv(TARGET_AWS_PROFILE)' -i "$HOME/.kube/config"
+                echo "Add env AWS_PROFILE value to default kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[1].name) = "AWS_REGION"' -i "$HOME/.kube/config"
+                echo "Add env AWS_REGION to default kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[1].value) = strenv(TARGET_AWS_REGION)' -i "$HOME/.kube/config"
+                echo "Add env AWS_REGION value to default kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[0].name) = "AWS_PROFILE"' -i "$HOME/.kube/$CSP-target-$DEPLOYMENT-kubeconfig"
+                echo "Add env AWS_PROFILE to deployment kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[0].value) = strenv(TARGET_AWS_PROFILE)' -i "$HOME/.kube/$CSP-target-$DEPLOYMENT-kubeconfig"
+                echo "Add env AWS_PROFILE value to deployment kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[1].name) = "AWS_REGION"' -i "$HOME/.kube/$CSP-target-$DEPLOYMENT-kubeconfig"
+                echo "Add env AWS_REGION to deployment kubeconfig - Result: $?"
+                yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[1].value) = strenv(TARGET_AWS_REGION)' -i "$HOME/.kube/$CSP-target-$DEPLOYMENT-kubeconfig"
+                echo "Add env AWS_REGION value to deployment kubeconfig - Result: $?"
+            done
         fi
-        echo "Found yq: $(command -v yq)"
-        CLUSTERS=$(aws eks list-clusters --profile=$TARGET_AWS_PROFILE --region=$TARGET_AWS_REGION)
-        for CLUSTER in $(echo $CLUSTERS | jq -r --arg DEPLOYMENT "$DEPLOYMENT" '.clusters[] | select(endswith((["-",$DEPLOYMENT]|join(""))))'); do
-            echo "Found cluster: $CLUSTER"
-            aws eks update-kubeconfig --profile=$TARGET_AWS_PROFILE --name="$CLUSTER" --region=$TARGET_AWS_REGION
-            aws eks update-kubeconfig --profile=$TARGET_AWS_PROFILE --name="$CLUSTER" --region=$TARGET_AWS_REGION --kubeconfig="$HOME/.kube/$CSP-target-$DEPLOYMENT-kubeconfig"
-            yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[0].name) = "AWS_PROFILE"' -i "$HOME/.kube/config"
-            echo "Add env AWS_PROFILE to default kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[0].value) = strenv(TARGET_AWS_PROFILE)' -i "$HOME/.kube/config"
-            echo "Add env AWS_PROFILE value to default kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[1].name) = "AWS_REGION"' -i "$HOME/.kube/config"
-            echo "Add env AWS_REGION to default kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[1].value) = strenv(TARGET_AWS_REGION)' -i "$HOME/.kube/config"
-            echo "Add env AWS_REGION value to default kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[0].name) = "AWS_PROFILE"' -i "$HOME/.kube/$CSP-target-$DEPLOYMENT-kubeconfig"
-            echo "Add env AWS_PROFILE to deployment kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[0].value) = strenv(TARGET_AWS_PROFILE)' -i "$HOME/.kube/$CSP-target-$DEPLOYMENT-kubeconfig"
-            echo "Add env AWS_PROFILE value to deployment kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[1].name) = "AWS_REGION"' -i "$HOME/.kube/$CSP-target-$DEPLOYMENT-kubeconfig"
-            echo "Add env AWS_REGION to deployment kubeconfig - Result: $?"
-            yq -i -r '(.users[] | select(.name | test(map("-", strenv(DEPLOYMENT), "$") | join(""))) | .user.exec.env[1].value) = strenv(TARGET_AWS_REGION)' -i "$HOME/.kube/$CSP-target-$DEPLOYMENT-kubeconfig"
-            echo "Add env AWS_REGION value to deployment kubeconfig - Result: $?"
-        done
     fi
-fi
-for CONFIG_FILE in "${CONFIG_FILES[@]}"; do
-    infomsg "Creating kubeconfig: $HOME/.kube/$CONFIG_FILE"
-    touch "$HOME/.kube/$CONFIG_FILE"
-done
+    for CONFIG_FILE in "${CONFIG_FILES[@]}"; do
+        infomsg "Creating kubeconfig: $HOME/.kube/$CONFIG_FILE"
+        touch "$HOME/.kube/$CONFIG_FILE"
+    done
+}
 
 # truncate the log file
 truncate -s0 $LOGFILE
+
+# ensure user session (sso) is valid and refresh as required
+check_session
+
+# prep kubeconfig files for kubernetes provider
+stage_kubeconfig
 
 # ensure formatting
 terraform fmt -no-color
