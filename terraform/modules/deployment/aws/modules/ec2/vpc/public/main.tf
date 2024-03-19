d6522fc04b5eeacc535331b591e40c3e4014b1b1
credibleforce <jamiefrasermcmurray@gmail.com>
2024-03-18 19:26:41 -0700

    update route table

--- terraform/modules/deployment/aws/modules/ec2/vpc/public/main.tf
+++ terraform/modules/deployment/aws/modules/ec2/vpc/public/main.tf
@@ -44,11 +44,6 @@ resource "aws_subnet" "public" {
 resource "aws_route_table" "public" {
     vpc_id = aws_vpc.public.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.public.id
-    }
-
     tags = {
         Name = "default-public-internet-gw-route-${var.environment}-${var.deployment}"
         environment = var.environment
