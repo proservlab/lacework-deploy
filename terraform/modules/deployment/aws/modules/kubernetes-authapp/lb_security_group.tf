54a1ee9df92f519114caddfb1aacd32f91b2bf8a
credibleforce <jamiefrasermcmurray@gmail.com>
2024-04-06 12:43:46 -0700

    update

--- terraform/modules/deployment/aws/modules/kubernetes-authapp/lb_security_group.tf
+++ terraform/modules/deployment/aws/modules/kubernetes-authapp/lb_security_group.tf
@@ -92,7 +92,7 @@ resource "aws_security_group_rule" "internet_exposed" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = "0.0.0.0/0"
+  cidr_blocks       = ["0.0.0.0/0"]
   description       = "Allow all tcp inbound from workstation, attacker and target public ips"
   security_group_id = aws_security_group.this.id
 
