3bfa0a674939b577e1b6801cb615e9e668975d26
credibleforce <jamiefrasermcmurray@gmail.com>
2024-04-17 11:16:00 -0700

    fix kubernetes service dns name output

--- terraform/modules/deployment/aws/modules/kubernetes-authapp/outputs.tf
+++ terraform/modules/deployment/aws/modules/kubernetes-authapp/outputs.tf
@@ -4,7 +4,7 @@ locals {
             name = local.app_name
             namespace = local.app_namespace
             hostname = kubernetes_service_v1.this.status[0].load_balancer[0].ingress[0].hostname
-            dynu_dns_name = var.enable_dynu_dns ? "${local.app_name}.${coalesce(var.dynu_dns_domain, "unknown")}" : null
+            dynu_dns_name = var.enable_dynu_dns ? "${local.app_name}-${var.environment}-${var.deployment}.${coalesce(var.dynu_dns_domain, "unknown")}" : null
             ip = null
             port = kubernetes_service_v1.this.spec[0].port[0].port
         }
