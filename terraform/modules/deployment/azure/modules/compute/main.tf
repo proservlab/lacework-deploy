162a2d8b8ea941e674d980f187c5aeb2d5ef4d64
Credibleforce <58786780+credibleforce@users.noreply.github.com>
2024-07-18 10:28:52 -0700

    fix default linux server name

--- terraform/modules/deployment/azure/modules/compute/main.tf
+++ terraform/modules/deployment/azure/modules/compute/main.tf
@@ -237,7 +237,7 @@ resource "azurerm_role_assignment" "system-identity-role" {
 # Create the linux virtual machine
 resource "azurerm_linux_virtual_machine" "instances" {
     for_each              = { for instance in var.instances: instance.name => instance if instance.role == "default" }
-    name                  = "${each.key}-identity-${var.environment}-${var.deployment}"
+    name                  = "${each.key}-${var.environment}-${var.deployment}"
     location              = var.region
     resource_group_name   = var.resource_group.name
     network_interface_ids = [azurerm_network_interface.nic[each.key].id]
