e957415e6a766aa40f973d45aec36064f9412f0c
Credibleforce <58786780+credibleforce@users.noreply.github.com>
2024-07-18 19:11:34 -0700

    fix missing ssh key

--- terraform/modules/deployment/azure/modules/compute/ssh.tf
+++ terraform/modules/deployment/azure/modules/compute/ssh.tf
@@ -6,6 +6,13 @@ locals {
     ssh_key_path = pathexpand("~/.ssh/azure-${var.environment}-${var.deployment}.pem")
 }
 
+# ssh key
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
+}
+
+# ssh key local path
 resource "local_file" "ssh-key" {
     content  = tls_private_key.ssh.private_key_pem
     filename = local.ssh_key_path
