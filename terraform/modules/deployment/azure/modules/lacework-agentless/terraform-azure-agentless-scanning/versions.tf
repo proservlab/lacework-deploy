e629330ef0bed889b4d93f6d765eb1adbbd2731d
Credibleforce <58786780+credibleforce@users.noreply.github.com>
2024-07-23 19:20:31 -0700

    update azure agentless azurerm version limit

--- terraform/modules/deployment/azure/modules/lacework-agentless/terraform-azure-agentless-scanning/versions.tf
+++ terraform/modules/deployment/azure/modules/lacework-agentless/terraform-azure-agentless-scanning/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.45.0"
+      version = "~> 2.45"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
