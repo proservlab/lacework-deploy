08ae7845e0e8d43599fad95b05d3b39772f245fd
credibleforce <58786780+credibleforce@users.noreply.github.com>
2024-10-23 13:56:36 -0700

    fix bucket reference

--- terraform/modules/deployment/aws/modules/lacework-s3-data-export/main.tf
+++ terraform/modules/deployment/aws/modules/lacework-s3-data-export/main.tf
@@ -59,6 +59,6 @@ data "aws_iam_role" "read_only_roles" {
 # Attach the S3 read policy to the user
 resource "aws_iam_role_policy_attachment" "lacework_user_s3_data_export_read_policy_attachment" {
   for_each = { for i in data.aws_iam_role.read_only_roles: i.name => i }
-  user       = each.key
+  role       = each.key
   policy_arn = aws_iam_policy.lacework_s3_data_export_read_policy[0].arn
 }
