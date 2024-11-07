264eae8ef7c0f6c77604e83419ac8bcf47f89eb7
credibleforce <58786780+credibleforce@users.noreply.github.com>
2024-11-07 10:58:56 -0800

    update lacework provider versions

--- terraform/modules/deployment/aws/modules/lacework-s3-data-export/main.tf
+++ terraform/modules/deployment/aws/modules/lacework-s3-data-export/main.tf
@@ -1,6 +1,6 @@
 module "lacework_s3_data_export" {
   source  = "lacework/s3-data-export/aws"
-  version = "1.3.3"
+  version = "~> 1.3"
 
   lacework_data_export_rule_name        = "AWS S3 Data Export"
   lacework_data_export_rule_description = "AWS S3 Data Export"
