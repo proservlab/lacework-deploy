a938a5aa78478550678f67070be34c0ee36ea7b1
credibleforce <58786780+credibleforce@users.noreply.github.com>
2024-11-14 10:07:22 -0800

    fix choco install

--- terraform/modules/deployment/common/payloads/windows/delayed_start.ps1
+++ terraform/modules/deployment/common/payloads/windows/delayed_start.ps1
@@ -56,7 +56,7 @@ function Install-ChocolateyIfNeeded {
         Write-Log "Chocolatey not found. Installing..."
         Set-ExecutionPolicy Bypass -Scope Process -Force
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
-        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     } else {
         Write-Log "Chocolatey is already installed."
     }
